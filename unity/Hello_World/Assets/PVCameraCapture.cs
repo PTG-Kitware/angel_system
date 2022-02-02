cac509cb244171ded5d9ba499d738053fd24a019
Josh Anderson <josh.anderson@kitware.com>
2022-02-02 14:05:03 -0600

    Add RGB camera streaming

---
+++ unity/Hello_World/Assets/PVCameraCapture.cs
@@ -0,0 +1,294 @@
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Windows.WebCam;
+using DilmerGames.Core.Singletons;
+using TMPro;
+using System.Runtime.InteropServices;
+
+#if ENABLE_WINMD_SUPPORT
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Media.Capture.Frames;
+using Windows.Media.MediaProperties;
+using System.Runtime.InteropServices.WindowsRuntime;
+#endif
+
+
+public class PVCameraCapture : MonoBehaviour
+{
+
+#if ENABLE_WINMD_SUPPORT
+    MediaCapture mediaCapture = null;
+    private MediaFrameReader frameReader = null;
+    private byte[] frameData = null;
+#endif
+
+    // Network stuff
+    System.Net.Sockets.TcpClient tcpClient;
+    NetworkStream tcpStream;
+
+    GameObject loggerObject = null;
+
+    long prev_ts;
+    uint framesRcvd;
+    string debugString = "";
+
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    unsafe interface IMemoryBufferByteAccess
+    {
+        /// <summary>
+        /// Unsafe function to retrieve the pointer and size information of the underlying
+        /// buffer object. Must be used within unsafe functions. In addition, the project needs
+        /// to be configured as "Allow unsafe code". [internal use]
+        /// </summary>
+        /// <param name="buffer">byte pointer to the start of the buffer</param>
+        /// <param name="capacity">the size of the buffer</param>
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
+
+    // Start is called before the first frame update
+    async void Start()
+    {
+        this.loggerObject = GameObject.Find("Logger");
+
+        // Connect to the python TCP server
+        this.tcpClient = new System.Net.Sockets.TcpClient();
+        try
+        {
+            this.tcpClient.Connect("169.254.103.120", 11004);
+            this.loggerObject.GetComponent<Logger>().LogInfo("TCP client PV connected!");
+            this.tcpStream = this.tcpClient.GetStream();
+        }
+        catch (Exception e)
+        {
+            this.loggerObject.GetComponent<Logger>().LogInfo(e.ToString());
+        }
+
+#if ENABLE_WINMD_SUPPORT
+        await InitializeMediaCaptureAsyncTask();
+
+        MediaFrameReaderStartStatus mediaFrameReaderStartStatus = await frameReader.StartAsync();
+        if (!(mediaFrameReaderStartStatus == MediaFrameReaderStartStatus.Success))
+        {
+		    this.loggerObject.GetComponent<Logger>().LogInfo("StartFrameReaderAsyncTask() is not successful, status = " + mediaFrameReaderStartStatus);
+		}
+
+#endif
+    }
+
+    void Update()
+    {
+        if (debugString != "")
+        {
+            this.loggerObject.GetComponent<Logger>().LogInfo(debugString);
+        }
+    }
+
+
+#if ENABLE_WINMD_SUPPORT
+    public async Task<bool> InitializeMediaCaptureAsyncTask()
+    {
+        int targetVideoWidth, targetVideoHeight;
+        float targetVideoFrameRate;
+        //targetVideoWidth = 1280;
+        //targetVideoHeight = 720;
+        targetVideoWidth = 1920;
+        targetVideoHeight = 1080;
+        targetVideoFrameRate = 30.0f;
+
+        var allGroups = await MediaFrameSourceGroup.FindAllAsync();
+        int selectedGroupIndex = -1;
+        for (int i = 0; i < allGroups.Count; i++)
+        {
+            var group = allGroups[i];
+            //this.loggerObject.GetComponent<Logger>().LogInfo(group.DisplayName + ", " + group.Id + " " + allGroups.Count.ToString());
+            this.loggerObject.GetComponent<Logger>().LogInfo(group.DisplayName);
+            if (group.DisplayName == "QC Back Camera")
+            {
+                selectedGroupIndex = i;
+                this.loggerObject.GetComponent<Logger>().LogInfo("Selected group " + i + " on HoloLens 2");
+                break;
+            }
+        }
+
+        if (selectedGroupIndex == -1)
+        {
+            this.loggerObject.GetComponent<Logger>().LogInfo("InitializeMediaCaptureAsyncTask() fails because there is no suitable source group");
+            return false;
+        }
+
+        // Initialize mediacapture with the source group.
+        mediaCapture = new MediaCapture();
+        MediaStreamType mediaStreamType = MediaStreamType.VideoPreview;
+
+        string deviceId = allGroups[selectedGroupIndex].Id;
+
+        // Look up for all video profiles
+        IReadOnlyList<MediaCaptureVideoProfile> profileList = MediaCapture.FindKnownVideoProfiles(deviceId, KnownVideoProfile.VideoConferencing);
+
+        // Initialize mediacapture with the source group.
+        var settings = new MediaCaptureInitializationSettings
+        {
+            SourceGroup = allGroups[selectedGroupIndex],
+            VideoDeviceId = deviceId,
+            VideoProfile = profileList[0],
+
+            // This media capture can share streaming with other apps.
+            SharingMode = MediaCaptureSharingMode.ExclusiveControl,
+
+            // Only stream video and don't initialize audio capture devices.
+            StreamingCaptureMode = StreamingCaptureMode.Video,
+
+            // Set to CPU to ensure frames always contain CPU SoftwareBitmap images
+            // instead of preferring GPU D3DSurface images.
+            MemoryPreference = MediaCaptureMemoryPreference.Cpu
+        };
+        
+        try
+        {
+            await mediaCapture.InitializeAsync(settings);
+
+            var mediaFrameSourceVideo = mediaCapture.FrameSources.Values.Single(x => x.Info.MediaStreamType == mediaStreamType);
+            MediaFrameFormat targetResFormat = null;
+            float framerateDiffMin = 60f;
+            foreach (var f in mediaFrameSourceVideo.SupportedFormats.OrderBy(x => x.VideoFormat.Width * x.VideoFormat.Height))
+            {
+                //this.loggerObject.GetComponent<Logger>().LogInfo("Format width: " + f.VideoFormat.Width.ToString());
+                //this.loggerObject.GetComponent<Logger>().LogInfo("Format height: " + f.VideoFormat.Height.ToString());
+
+                if (f.VideoFormat.Width == targetVideoWidth && f.VideoFormat.Height == targetVideoHeight)
+                {
+                    if (targetResFormat == null)
+                    {
+                        this.loggerObject.GetComponent<Logger>().LogInfo("Found matching format");
+                        targetResFormat = f;
+                        framerateDiffMin = Mathf.Abs(f.FrameRate.Numerator / f.FrameRate.Denominator - targetVideoFrameRate);
+                    }
+                    else if (Mathf.Abs(f.FrameRate.Numerator / f.FrameRate.Denominator - targetVideoFrameRate) < framerateDiffMin)
+                    {
+                        targetResFormat = f;
+                        this.loggerObject.GetComponent<Logger>().LogInfo("Else?");
+                        framerateDiffMin = Mathf.Abs(f.FrameRate.Numerator / f.FrameRate.Denominator - targetVideoFrameRate);
+                    }
+                }
+            }
+            if (targetResFormat == null)
+            {
+                targetResFormat = mediaFrameSourceVideo.SupportedFormats[0];
+                this.loggerObject.GetComponent<Logger>().LogInfo("Unable to choose the selected format, fall back");
+                targetResFormat = mediaFrameSourceVideo.SupportedFormats.OrderBy(x => x.VideoFormat.Width * x.VideoFormat.Height).FirstOrDefault();
+            }
+
+            await mediaFrameSourceVideo.SetFormatAsync(targetResFormat);
+            this.loggerObject.GetComponent<Logger>().LogInfo("Sub type " + targetResFormat.Subtype);
+
+            frameReader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSourceVideo, targetResFormat.Subtype);
+            frameReader.FrameArrived += OnFrameArrived;
+
+            frameData = new byte[(int) (1920 * targetResFormat.VideoFormat.Height * 1.5) + 16];
+            this.loggerObject.GetComponent<Logger>().LogInfo("FrameReader is successfully initialized, " + targetResFormat.VideoFormat.Width + "x" + targetResFormat.VideoFormat.Height +
+                ", Framerate: " + targetResFormat.FrameRate.Numerator + "/" + targetResFormat.FrameRate.Denominator);
+        }
+        catch (Exception e)
+        {
+            this.loggerObject.GetComponent<Logger>().LogInfo("FrameReader is not initialized");
+            this.loggerObject.GetComponent<Logger>().LogInfo("Exception: " + e);
+            return false;
+        }
+
+        this.loggerObject.GetComponent<Logger>().LogInfo("InitializeMediaCaptureAsyncTask() is successful");
+
+        return true;
+    }
+
+    unsafe private void OnFrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+    {
+        try
+        {
+            using (var frame = sender.TryAcquireLatestFrame())
+            {
+                if (frame != null)
+                {
+                    /*
+                    float[] cameraToWorldMatrixAsFloat = null;                
+                    if (HL2TryGetCameraToWorldMatrix(frame, out cameraToWorldMatrixAsFloat) == false)
+                    {
+                        this.loggerObject.GetComponent<Logger>().LogInfo("HL2TryGetCameraToWorldMatrix failed");
+                        return;
+                    }
+
+                    latestLocatableCameraToWorld = ConvertFloatArrayToMatrix4x4(cameraToWorldMatrixAsFloat);
+                    */
+
+                    var originalSoftwareBitmap = frame.VideoMediaFrame.SoftwareBitmap;
+
+                    //debugString += "height: " + newSoftwareBitmap.PixelHeight.ToString() + "\n";
+                    //debugString += "width: " + newSoftwareBitmap.PixelWidth.ToString() + "\n";
+                    //debugString += "pixel f: " + newSoftwareBitmap.BitmapPixelFormat.ToString() + "\n";
+
+                    using (var input = originalSoftwareBitmap.LockBuffer(BitmapBufferAccessMode.Read))
+                    using (var inputReference = input.CreateReference())
+                    {
+                        byte* inputBytes;
+                        uint inputCapacity;
+                        ((IMemoryBufferByteAccess)inputReference).GetBuffer(out inputBytes, out inputCapacity);
+
+                        //debugString = "capacity: " + inputCapacity.ToString() + "\n";
+                        //debugString += "alpha mode: " + originalSoftwareBitmap.BitmapAlphaMode.ToString() + "\n";
+                        //debugString = "height: " + originalSoftwareBitmap.PixelHeight.ToString() + "\n";
+                        //debugString += "width: " + originalSoftwareBitmap.PixelWidth.ToString() + "\n";
+
+                        // add header
+                        byte[] frameHeader = { 0x1A, 0xCF, 0xFC, 0x1D,
+                                        (byte)(((inputCapacity + 8) & 0xFF000000) >> 24),
+                                        (byte)(((inputCapacity + 8) & 0x00FF0000) >> 16),
+                                        (byte)(((inputCapacity + 8) & 0x0000FF00) >> 8),
+                                        (byte)(((inputCapacity + 8) & 0x000000FF) >> 0),
+                                        (byte)((originalSoftwareBitmap.PixelWidth & 0xFF000000) >> 24),
+                                        (byte)((originalSoftwareBitmap.PixelWidth & 0x00FF0000) >> 16),
+                                        (byte)((originalSoftwareBitmap.PixelWidth & 0x0000FF00) >> 8),
+                                        (byte)((originalSoftwareBitmap.PixelWidth & 0x000000FF) >> 0),
+                                        (byte)((originalSoftwareBitmap.PixelHeight & 0xFF000000) >> 24),
+                                        (byte)((originalSoftwareBitmap.PixelHeight & 0x00FF0000) >> 16),
+                                        (byte)((originalSoftwareBitmap.PixelHeight & 0x0000FF00) >> 8),
+                                        (byte)((originalSoftwareBitmap.PixelHeight & 0x000000FF) >> 0) };
+                        System.Buffer.BlockCopy(frameHeader, 0, frameData, 0, frameHeader.Length);
+
+                        Marshal.Copy((IntPtr)inputBytes, frameData, 16, (int)inputCapacity);
+
+                        // Send the data through the socket.  
+                        tcpStream.Write(frameData, 0, frameData.Length);
+                        tcpStream.Flush();
+
+                        originalSoftwareBitmap?.Dispose();
+                    }
+                    
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            debugString += ("Frame Arrived Exception: " + e);
+        }
+    }
+#endif
+
+}
--- unity/Hello_World/Assets/ResearchModeCapture.cs
+++ unity/Hello_World/Assets/ResearchModeCapture.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 #endif
 
 
-public class CaptureSensorData : MonoBehaviour
+public class ResearchModeCapture : MonoBehaviour
 {
 
 #if ENABLE_WINMD_SUPPORT
@@ -61,48 +61,15 @@ public class CaptureSensorData : MonoBehaviour
     System.Net.Sockets.TcpClient tcpClient4;
     NetworkStream tcpStream4;
 
-    private Logger _logger = null;
+    GameObject loggerObject = null;
 
     // Spatial awareness stuff
     IEnumerable<SpatialAwarenessMeshObject> meshes;
     IMixedRealitySpatialAwarenessMeshObserver observer = null;
 
     long prev_ts;
-    // IP Address hosting the server to connect to.
-    // USB-C :: Worked after opening up incoming port through the firewall.
-    string ip_address = "169.254.70.247";
-    int ip_port = 11000;
-
-    /// <summary>
-    /// Lazy acquire the logger object and return the reference to it.
-    /// </summary>
-    /// <returns>Logger instance reference.</returns>
-    private ref Logger logger()
-    {
-        if( this._logger == null )
-        {
-            // TODO: Error handling for null loggerObject?
-            this._logger = GameObject.Find("Logger").GetComponent<Logger>();
-        }
-        return ref this._logger;
-    }
     uint framesRcvd;
     string debugString = "";
-
-    [ComImport]
-    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-    unsafe interface IMemoryBufferByteAccess
-    {
-        /// <summary>
-        /// Unsafe function to retrieve the pointer and size information of the underlying
-        /// buffer object. Must be used within unsafe functions. In addition, the project needs
-        /// to be configured as "Allow unsafe code". [internal use]
-        /// </summary>
-        /// <param name="buffer">byte pointer to the start of the buffer</param>
-        /// <param name="capacity">the size of the buffer</param>
-        void GetBuffer(out byte* buffer, out uint capacity);
-    }
 
     private void Awake()
     {
@@ -114,30 +81,7 @@ public class CaptureSensorData : MonoBehaviour
     // Start is called before the first frame update
     async void Start()
     {
-        Logger log = logger();
-
-<<<<<<< HEAD
-#if ENABLE_WINMD_SUPPORT
-        // Configure research mode
-        log.LogInfo("Trying to enable research mode...");
-        researchMode = new HL2ResearchMode();
-        log.LogInfo("Research mode enabled");
-
-        // Depth sensor should be initialized in only one mode
-        if (depthSensorMode == DepthSensorMode.LongThrow) researchMode.InitializeLongDepthSensor();
-        else if (depthSensorMode == DepthSensorMode.ShortThrow) researchMode.InitializeDepthSensor();
-
-        researchMode.InitializeSpatialCamerasFront();
-        researchMode.SetReferenceCoordinateSystem(unityWorldOrigin);
-        researchMode.SetPointCloudDepthOffset(0);
-
-        // Depth sensor should be initialized in only one mode
-        if (depthSensorMode == DepthSensorMode.LongThrow) researchMode.StartLongDepthSensorLoop(enablePointCloud);
-        else if (depthSensorMode == DepthSensorMode.ShortThrow) researchMode.StartDepthSensorLoop(enablePointCloud);
-
-        researchMode.StartSpatialCamerasFrontLoop();
-        log.LogInfo("Research mode initialized");
-#endif
+        this.loggerObject = GameObject.Find("Logger");
 
         // Connect to the python TCP servers
         this.tcpClient1 = new System.Net.Sockets.TcpClient();
@@ -146,31 +90,28 @@ public class CaptureSensorData : MonoBehaviour
         this.tcpClient4 = new System.Net.Sockets.TcpClient();
         try
         {
-            /*
-            this.tcpClient1.Connect(ip_address, 11000);
-            log.LogInfo("TCP client 1 connected!");
+            this.tcpClient1.Connect("169.254.103.120", 11000);
+            this.loggerObject.GetComponent<Logger>().LogInfo("TCP client 1 connected!");
             this.tcpStream1 = this.tcpClient1.GetStream();
 
-            this.tcpClient2.Connect(ip_address, 11001);
-            log.LogInfo("TCP client 2 connected!");
+            this.tcpClient2.Connect("169.254.103.120", 11001);
+            this.loggerObject.GetComponent<Logger>().LogInfo("TCP client 2 connected!");
             this.tcpStream2 = this.tcpClient2.GetStream();
 
-            this.tcpClient3.Connect(ip_address, 11002);
-            log.LogInfo("TCP client 3 connected!");
+            this.tcpClient3.Connect("169.254.103.120", 11002);
+            this.loggerObject.GetComponent<Logger>().LogInfo("TCP client 3 connected!");
             this.tcpStream3 = this.tcpClient3.GetStream();
 
-            this.tcpClient4.Connect(ip_address, 11003);
-            log.LogInfo("TCP client 4 connected!");
+            this.tcpClient4.Connect("169.254.103.120", 11003);
+            this.loggerObject.GetComponent<Logger>().LogInfo("TCP client 4 connected!");
             this.tcpStream4 = this.tcpClient4.GetStream();
-            */
         }
         catch (Exception e)
         {
-            log.LogInfo(e.ToString());
+            this.loggerObject.GetComponent<Logger>().LogInfo(e.ToString());
         }
 
 #if ENABLE_WINMD_SUPPORT
-        /*
         // Configure research mode
         this.loggerObject.GetComponent<Logger>().LogInfo("Research mode enabled");
         researchMode = new HL2ResearchMode();
@@ -178,7 +119,7 @@ public class CaptureSensorData : MonoBehaviour
         // Depth sensor should be initialized in only one mode
         if (depthSensorMode == DepthSensorMode.LongThrow) researchMode.InitializeLongDepthSensor();
         else if (depthSensorMode == DepthSensorMode.ShortThrow) researchMode.InitializeDepthSensor();
-
+        
         researchMode.InitializeSpatialCamerasFront();
         researchMode.SetReferenceCoordinateSystem(unityWorldOrigin);
         researchMode.SetPointCloudDepthOffset(0);
@@ -189,9 +130,7 @@ public class CaptureSensorData : MonoBehaviour
 
         researchMode.StartSpatialCamerasFrontLoop();
         this.loggerObject.GetComponent<Logger>().LogInfo("Research mode initialized");
-        */
 
-        /*
         // Start the publishing thread
         Thread tLFCameraThread = new Thread(LFCameraThread);
         tLFCameraThread.Start();
@@ -201,7 +140,6 @@ public class CaptureSensorData : MonoBehaviour
         tLLCameraThread.Start();
         Thread tRRCameraThread = new Thread(RRCameraThread);
         tRRCameraThread.Start();
-        */
 #endif
     }
 
@@ -219,8 +157,8 @@ public class CaptureSensorData : MonoBehaviour
                     observer.DisplayOption = SpatialAwarenessMeshDisplayOptions.None;
                     //observer.LevelOfDetail = SpatialAwarenessMeshLevelOfDetail.Unlimited;
                     //observer.UpdateInterval = 0.5f;
-                    this.logger().LogInfo("Detail level: " + observer.LevelOfDetail.ToString());
-                    this.logger().LogInfo("Update interval: " + observer.UpdateInterval.ToString());
+                    this.loggerObject.GetComponent<Logger>().LogInfo("Detail level: " + observer.LevelOfDetail.ToString());
+                    this.loggerObject.GetComponent<Logger>().LogInfo("Update interval: " + observer.UpdateInterval.ToString());
                 }
             }
         }
@@ -234,6 +172,7 @@ public class CaptureSensorData : MonoBehaviour
     public void LFCameraThread()
     {
 #if ENABLE_WINMD_SUPPORT
+        bool sendImage = true;
         while (true)
         {
             // Try to get the frame from research mode
@@ -242,6 +181,12 @@ public class CaptureSensorData : MonoBehaviour
                 long ts;
                 byte [] framePayload = researchMode.GetLFCameraBuffer(out ts);
 
+                // only send every other image
+                if (!(sendImage))
+                {
+                    sendImage = true;
+                    continue;
+                }
                 //debugString = (ts - prev_ts).ToString();
 
                 //prev_ts = ts;
@@ -269,13 +214,14 @@ public class CaptureSensorData : MonoBehaviour
                     System.Buffer.BlockCopy(frameHeader, 0, frame, 0, frameHeader.Length);
                     System.Buffer.BlockCopy(framePayload, 0, frame, frameHeader.Length, framePayload.Length);
 
-                    // Send the data through the socket.
+                    // Send the data through the socket.  
                     tcpStream1.Write(frame, 0, frame.Length);
                     tcpStream1.Flush();
+                    //sendImage = false;
                 } // end if length > 0
             } // end if image available
 
-            Thread.Sleep(1);
+            Thread.Sleep(3);
         } // end while loop
 #endif
     } // end method
@@ -283,6 +229,8 @@ public class CaptureSensorData : MonoBehaviour
     public void RFCameraThread()
     {
 #if ENABLE_WINMD_SUPPORT
+        bool sendImage = true;
+
         while (true)
         {
             // Try to get the frame from research mode
@@ -291,6 +239,12 @@ public class CaptureSensorData : MonoBehaviour
                 long ts;
                 byte [] framePayload = researchMode.GetRFCameraBuffer(out ts);
 
+                // only send every other image
+                if (!(sendImage))
+                {
+                    sendImage = true;
+                    continue;
+                }
                 //debugString = (ts - prev_ts).ToString();
 
                 //prev_ts = ts;
@@ -318,13 +272,15 @@ public class CaptureSensorData : MonoBehaviour
                     System.Buffer.BlockCopy(frameHeader, 0, frame, 0, frameHeader.Length);
                     System.Buffer.BlockCopy(framePayload, 0, frame, frameHeader.Length, framePayload.Length);
 
-                    // Send the data through the socket.
+                    // Send the data through the socket.  
                     tcpStream2.Write(frame, 0, frame.Length);
                     tcpStream2.Flush();
+                    //sendImage = false;
+
                 } // end if length > 0
             } // end if image available
 
-            Thread.Sleep(1);
+            Thread.Sleep(3);
         } // end while loop
 #endif
     } // end method
@@ -332,6 +288,8 @@ public class CaptureSensorData : MonoBehaviour
     public void LLCameraThread()
     {
 #if ENABLE_WINMD_SUPPORT
+        bool sendImage = true;
+
         while (true)
         {
             // Try to get the frame from research mode
@@ -341,7 +299,12 @@ public class CaptureSensorData : MonoBehaviour
                 byte [] framePayload = researchMode.GetLLCameraBuffer(out ts);
 
                 //debugString = (ts - prev_ts).ToString();
-
+                // only send every other image
+                if (!(sendImage))
+                {
+                    sendImage = true;
+                    continue;
+                }
                 //prev_ts = ts;
                 if (framePayload.Length > 0)
                 {
@@ -367,13 +330,16 @@ public class CaptureSensorData : MonoBehaviour
                     System.Buffer.BlockCopy(frameHeader, 0, frame, 0, frameHeader.Length);
                     System.Buffer.BlockCopy(framePayload, 0, frame, frameHeader.Length, framePayload.Length);
 
-                    // Send the data through the socket.
+                    // Send the data through the socket.  
                     tcpStream3.Write(frame, 0, frame.Length);
                     tcpStream3.Flush();
+
+                    //sendImage = false;
+
                 } // end if length > 0
             } // end if image available
 
-            Thread.Sleep(1);
+            Thread.Sleep(3);
         } // end while loop
 #endif
     } // end method
@@ -381,6 +347,8 @@ public class CaptureSensorData : MonoBehaviour
     public void RRCameraThread()
     {
 #if ENABLE_WINMD_SUPPORT
+        bool sendImage = true;
+
         while (true)
         {
             // Try to get the frame from research mode
@@ -388,6 +356,13 @@ public class CaptureSensorData : MonoBehaviour
             {
                 long ts;
                 byte [] framePayload = researchMode.GetRRCameraBuffer(out ts);
+
+                // only send every other image
+                if (!(sendImage))
+                {
+                    sendImage = true;
+                    continue;
+                }
 
                 //debugString = (ts - prev_ts).ToString();
 
@@ -416,13 +391,14 @@ public class CaptureSensorData : MonoBehaviour
                     System.Buffer.BlockCopy(frameHeader, 0, frame, 0, frameHeader.Length);
                     System.Buffer.BlockCopy(framePayload, 0, frame, frameHeader.Length, framePayload.Length);
 
-                    // Send the data through the socket.
+                    // Send the data through the socket.  
                     tcpStream4.Write(frame, 0, frame.Length);
                     tcpStream4.Flush();
+                    //sendImage = false;
                 } // end if length > 0
             } // end if image available
 
-            Thread.Sleep(1);
+            Thread.Sleep(3);
         } // end while loop
 #endif
     } // end method
