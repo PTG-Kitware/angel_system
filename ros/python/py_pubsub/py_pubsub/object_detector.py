af07ebe4b5b85b850ef8d32dd5aec836c6715847
Josh Anderson <josh.anderson@kitware.com>
2022-02-16 09:42:12 -0600

    Minor cleanup

--- ros/python/py_pubsub/py_pubsub/object_detector.py
+++ ros/python/py_pubsub/py_pubsub/object_detector.py
@@ -8,7 +8,6 @@ import threading
 import numpy as np
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, ByteMultiArray
 from sensor_msgs.msg import Image
 import cv2
 import PIL
@@ -22,71 +21,72 @@ TOPICS = ["PVFrames"]
 
 class ObjectDetector(Node):
 
-    def __init__(self, topic):
-        super().__init__('object_detector')
+    def __init__(self,
+        topic: str,
+        node_name: str = None,
+        detection_threshold: float = 0.8,
+    ):
+        if node_name is None:
+            node_name = self.__class__.__name__
+        super().__init__(node_name)
 
         if topic not in TOPICS:
             print("Error! Invalid topic name")
             sys.exit()
 
-        self.topic = topic
+        self._topic = topic
+        self._detection_threshold = detection_threshold
 
-        self.subscription = self.create_subscription(
+        self._subscription = self.create_subscription(
             Image,
-            self.topic,
+            self._topic,
             self.listener_callback,
             100)
-        self.subscription  # prevent unused variable warning
+        self._subscription  # prevent unused variable warning
 
-        self.frames_recvd = 0
-        self.prev_time = -1
+        self._frames_recvd = 0
+        self._prev_time = -1
 
         # instantiate detector
-        self.detector = ResNetFRCNN(img_batch_size=1)
-        print("Ready to detect", self.detector, self.detector.use_cuda)
-        self.images = []
+        self._detector = ResNetFRCNN(img_batch_size=1)
+        print("Ready to detect", self._detector, self._detector.use_cuda)
+        self._images = []
 
-        self.ax1 = plot.subplot(111)
-        self.im1 = self.ax1.imshow(np.zeros(shape=(720, 1280, 3)), vmin=0, vmax=255)
+        self._ax1 = plot.subplot(111)
+        self._im1 = self._ax1.imshow(np.zeros(shape=(720, 1280, 3)), vmin=0, vmax=255)
 
         plot.ion()
         plot.show()
 
 
-    def listener_callback(self, msg):
-        self.frames_recvd += 1
-        if self.prev_time == -1:
-            self.prev_time = time.time()
-        elif (time.time() - self.prev_time > 1):
-            print("Frames rcvd", self.frames_recvd)
-            #print(msg.header, msg.height, msg.width, msg.encoding, len(msg.data), msg.data[0:10])
-            self.frames_recvd = 0
-            self.prev_time = time.time()
+    def listener_callback(self, image):
+        self._frames_recvd += 1
+        if self._prev_time == -1:
+            self._prev_time = time.time()
+        elif (time.time() - self._prev_time > 1):
+            print("Frames rcvd", self._frames_recvd)
+            self._frames_recvd = 0
+            self._prev_time = time.time()
 
-        yuv_data = np.frombuffer(msg.data, np.uint8).reshape(msg.height*3//2, msg.width)
-        rgb = cv2.cvtColor(yuv_data, cv2.COLOR_YUV2RGB_NV12);
-        source_img = PIL.Image.fromarray(rgb)
-
+        # convert NV12 image to RGB
+        yuv_image = np.frombuffer(image.data, np.uint8).reshape(image.height*3//2, image.width)
+        rgb_image = cv2.cvtColor(yuv_image, cv2.COLOR_YUV2RGB_NV12)
+        pil_image = PIL.Image.fromarray(rgb_image)
         #print(type(rgb), rgb.size, rgb.shape)
+        #print("image_np stuff", rgb.shape, rgb.dtype, image.height, image.width, rgb[0:2])
 
-        # convert to np-array
-        #print("image_np stuff", rgb.shape, rgb.dtype, msg.height, msg.width, rgb[0:2])
-
-        image_np = (rgb / 255.0).astype(np.float32)
+        # convert np array to tensor
+        image_np = (rgb_image / 255.0).astype(np.float32)
         image_tensor = torch.from_numpy(image_np.transpose(2, 0, 1))
-
         transform = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
-
         image_tensor = transform(image_tensor)
 
-        #print("image!", image_tensor[0][0], image_np.shape)
-        self.images.append(image_tensor)
-
-        if len(self.images) >= 1:
+        self._images.append(image_tensor)
+        if len(self._images) >= 1:
             # send to detector
             start = time.time()
-            detections = self.detector.detect_objects(self.images)
+            detections = self._detector.detect_objects(self._images)
             end = time.time()
             print("Time to perform detections", end - start)
 
@@ -100,10 +100,10 @@ class ObjectDetector(Node):
                     class_dict_sorted = {k: v for k, v in sorted(class_dict.items(), key=lambda item: item[1], reverse=True)}
                     #print("Results sorted:", list(class_dict_sorted.items())[:5])
 
-                    if list(class_dict_sorted.items())[0][1] > 0.80:
+                    if list(class_dict_sorted.items())[0][1] > self._detection_threshold:
                         print("Found something:", list(class_dict_sorted.items())[0], bounding_box)
 
-                        draw = PIL.ImageDraw.Draw(source_img)
+                        draw = PIL.ImageDraw.Draw(pil_image)
                         draw.rectangle(((bounding_box.min_vertex[0], bounding_box.min_vertex[1]),
                                         (bounding_box.max_vertex[0], bounding_box.max_vertex[1])),
                                         outline="black", width=10)
@@ -112,13 +112,12 @@ class ObjectDetector(Node):
                         #print("Results sorted:", list(class_dict_sorted.items())[:5])
                         #print(list(class_dict_sorted.items())[0][1])
                         pass
-            self.images = []
+            self._images = []
 
-        self.im1.set_data(source_img)
+        self._im1.set_data(pil_image)
 
         plot.gcf().canvas.flush_events()
         plot.show(block=False)
-
 
 
 def main():
