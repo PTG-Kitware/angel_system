81ab3607756aba7c8a2cb63eb440c64a733b1e24
Paul Tunison <paul.tunison@kitware.com>
2022-03-07 12:52:19 -0500

    Added some docker tooling to push/pull from a gitlab contianer registry
    Using kitware.gitlab.com currently for storage.

--- angel-docker-pull.sh
+++ angel-docker-pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Build the docker images.
+# Pull the docker images.
 #
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
@@ -9,13 +9,12 @@ pushd "$SCRIPT_DIR"
 function usage()
 {
   echo "
-Usage: $0 [-h|--help] [--force]
+Usage: $0 [-h|--help]
 
-Build the PTG ANGEL system docker container images.
+Pull docker images from the registry configured via the environment parameters.
 
 Options:
   -h | --help   Display this message.
-  --force       Force image building regardless of workspace hygiene.f
 "
 }
 
@@ -28,29 +27,11 @@ do
       usage
       exit 0
       ;;
-    --force)
-      echo "Forcing build regardless of workspace hygiene."
-      shift
-      FORCE_BUILD=1
-      ;;
     *)  # anything else
       dc_forward_params+=("$1")
       shift
   esac
 done
-
-git_status="$(git status --porcelain "${SCRIPT_DIR}/ros")"
-if [[ -n "$git_status" ]]
-then
-  echo "WARNING: ROS workspace subtree is not clean."
-  if [[ -n "$FORCE_BUILD" ]]
-  then
-    echo "WARNING: Force enabled, building anyway."
-  else
-    echo "ERROR: Refusing to build images."
-    exit 1
-  fi
-fi
 
 if [[ "${#dc_forward_params[@]}" -gt 0 ]]
 then
@@ -62,4 +43,4 @@ docker-compose \
   --env-file "$SCRIPT_DIR"/docker/.env \
   -f "$SCRIPT_DIR"/docker/docker-compose.yml \
   --profile build-only \
-  build "$@"
+  pull "$@"
