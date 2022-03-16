35ca18a1d8dcb196e1e9031af8bd3d4ed1cf5a1e
Paul Tunison <paul.tunison@kitware.com>
2022-03-16 13:14:57 -0400

    Added a comment/note on building with debug symbols

--- docker/workspace_build.sh
+++ docker/workspace_build.sh
@@ -7,6 +7,9 @@
 #
 # Additional parameters are passed on to `colcon build`.
 #
+# For additional debugging of build products, try adding:
+#   --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
+#
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
