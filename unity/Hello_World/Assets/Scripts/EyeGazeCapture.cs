7f923b9330cadf8e3a09466bd812b6b577c02920
Josh Anderson <josh.anderson@kitware.com>
2022-05-24 09:32:53 -0500

    Publish EyeGazeMsg

--- unity/Hello_World/Assets/Scripts/EyeGazeCapture.cs
+++ unity/Hello_World/Assets/Scripts/EyeGazeCapture.cs
@@ -1,26 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
-
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.BuiltinInterfaces;
 using RosMessageTypes.Std;
-using RosMessageTypes.Sensor;
+using RosMessageTypes.Geometry;
 using RosMessageTypes.Angel;
 
 
 public class EyeGazeCapture : MonoBehaviour
 {
-    private float defaultDistanceInMeters = 2f;
-
     private Logger _logger = null;
+
+    // For filling in ROS message timestamp
+    DateTime timeOrigin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
     // Ros stuff
     ROSConnection ros;
-    public string eyeGazeTopic = "EyeGazeData";
+    public string eyeGazeTopicName = "EyeGazeData";
 
     /// <summary>
     /// Lazy acquire the logger object and return the reference to it.
@@ -40,8 +41,8 @@ public class EyeGazeCapture : MonoBehaviour
     void Start()
     {
         // Create the eye gaze publisher
-        //ros = ROSConnection.GetOrCreateInstance();
-        //ros.RegisterPublisher<HandJointPosesUpdateMsg>(eyeGazeTopic);
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<EyeGazeDataMsg>(eyeGazeTopicName);
     }
 
     // Update is called once per frame
@@ -52,15 +53,42 @@ public class EyeGazeCapture : MonoBehaviour
         var eyeGazeProvider = CoreServices.InputSystem?.EyeGazeProvider;
         if (eyeGazeProvider != null)
         {
-            //log.LogInfo("Eye valid: " + eyeGazeProvider.IsEyeTrackingEnabledAndValid.ToString());
-            //log.LogInfo("Eye data valid: " + eyeGazeProvider.IsEyeTrackingDataValid.ToString());
-            //log.LogInfo("Eye tracking enabled: " + eyeGazeProvider.IsEyeTrackingEnabled.ToString());
-            //log.LogInfo("Eye calibration valid: " + eyeGazeProvider.IsEyeCalibrationValid.ToString());
-            //log.LogInfo("Eye enabled: " + eyeGazeProvider.Enabled.ToString());
-            log.LogInfo("Head movement dir: " + eyeGazeProvider.HeadMovementDirection.ToString());
-            //log.LogInfo("Head velocity: " + eyeGazeProvider.HeadVelocity.ToString());
-            //log.LogInfo("Eye origin: " + eyeGazeProvider.GazeOrigin.ToString());
-            log.LogInfo("Eye direction: " + eyeGazeProvider.GazeDirection.ToString());
+            if (eyeGazeProvider.IsEyeTrackingEnabledAndValid)
+            {
+                // Get the latest eye tracking data and form it into a ROS message
+                PointMsg gazeOrigin = new PointMsg(eyeGazeProvider.GazeOrigin.x,
+                                                   eyeGazeProvider.GazeOrigin.y,
+                                                   eyeGazeProvider.GazeOrigin.z);
+                Vector3Msg gazeDirection = new Vector3Msg(eyeGazeProvider.GazeDirection.x,
+                                                          eyeGazeProvider.GazeDirection.y,
+                                                          eyeGazeProvider.GazeDirection.z);
+                Vector3Msg headMovementDirection = new Vector3Msg(eyeGazeProvider.HeadMovementDirection.x,
+                                                                  eyeGazeProvider.HeadMovementDirection.y,
+                                                                  eyeGazeProvider.HeadMovementDirection.z);
+                Vector3Msg headVelocity = new Vector3Msg(eyeGazeProvider.HeadVelocity.x,
+                                                         eyeGazeProvider.HeadVelocity.y,
+                                                         eyeGazeProvider.HeadVelocity.z);
+
+                // Create the ROS std_msgs header with the EyeGazeProvider's timestamps
+                var currTime = eyeGazeProvider.Timestamp;
+                TimeSpan diff = currTime.ToUniversalTime() - timeOrigin;
+                var sec = Convert.ToInt32(Math.Floor(diff.TotalSeconds));
+                var nsecRos = Convert.ToUInt32((diff.TotalSeconds - sec) * 1e9f);
+
+                HeaderMsg header = new HeaderMsg(
+                    new TimeMsg(sec, nsecRos),
+                    "EyeGazeData"
+                );
+
+                EyeGazeDataMsg eyeGazeDataMsg = new EyeGazeDataMsg(header, gazeOrigin, gazeDirection,
+                                                                   headMovementDirection, headVelocity);
+
+                ros.Publish(eyeGazeTopicName, eyeGazeDataMsg);
+
+                log.LogInfo("hit valid: " + eyeGazeProvider.HitInfo.raycastValid.ToString());
+                log.LogInfo("hit point: " + eyeGazeProvider.HitInfo.point.ToString());
+
+            }
         }
     }
 }
