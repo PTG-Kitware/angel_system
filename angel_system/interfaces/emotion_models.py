efba303c69741c1d3fae4504f3ac49bbd4112acb
Max Chen <mlc294@cornell.edu>
2022-06-30 17:20:46 -0400

    Emotion interface

---
+++ angel_system/interfaces/emotion_models.py
@@ -0,0 +1,37 @@
+import abc
+from typing import Iterable, Hashable, Dict, Tuple, Any
+import numpy as np
+
+from smqtk_core import Configurable, Pluggable
+
+
+class EmotionModel (Configurable, Pluggable):
+    """
+    Model to detect emotions from a set of input
+    2D ``numpy.ndarray`` type arrays.
+    """
+    @abc.abstractmethod
+    def __init__(self, model):
+        """
+        model: a serialized model. __init__ should implement loading model either using pickle or torch.
+        """
+        
+    @abc.abstractmethod
+    def classify(
+      self,
+      X: Iterable[np.ndarray]
+    ) -> Iterable[str]:
+        """
+        Classify in the given modality representation.
+        :param X: Iterable of some input modality as numpy arrays.
+        :return: Prediction output corresponding to class number
+        """
+
+    def __call__(
+      self,
+      X: Iterable[np.ndarray]
+    ) -> Iterable[str]:
+        """
+        Calls `classify_emotion() with the given iterable modality representation.`
+        """
+        return self.classify(X)
