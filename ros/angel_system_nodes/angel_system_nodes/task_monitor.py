621274e5b3a7404ef0843a437fea4273c759590f
Josh Anderson <josh.anderson@kitware.com>
2022-07-07 11:46:06 -0500

    Fix step order and num classes

--- ros/angel_system_nodes/angel_system_nodes/task_monitor.py
+++ ros/angel_system_nodes/angel_system_nodes/task_monitor.py
@@ -77,20 +77,21 @@ class CoffeeDemoTask():
 
         # TODO: make a labels.txt file for these steps
         self.steps = [
-            {'name': 'Pour_12oz_of_water_into_a_liquid_measuring_cup'},
+            {'name': 'Pour_12_ounces_of_water_into_liquid_measuring_cup'},
             {'name': 'Pour_water_from_the_liquid_measuring_cup_into_the_electric_kettle'},
             {'name': 'Turn_on_the_kettle'},
-            {'name': 'Turn_on_kitchen_scale'},
-            {'name': 'Place_bowl_on_scale'},
-            {'name': 'Zero_scale'},
-            {'name': 'Add_coffee_beans_until_scale_reads_25_grams'},
-            {'name': 'Pour_coffee_beans_into_coffee_grinder'},
+            # BOIL WATER STEP - Skipping for now
+            {'name': 'Place_the_dripper_on_top_of_the_mug'},
             {'name': 'Take_the_coffee_filter_and_fold_it_in_half_to_create_a_semi_circle'},
             {'name': 'Fold_the_filter_in_half_again_to_create_a_quarter_circle'},
             {'name': 'Place_the_folded_filter_into_the_dripper_such_that_the_point' +
                      '_of_the_quarter_circle_rests_in_the_center_of_the_dripper'},
             {'name': 'Spread_the_filter_open_to_create_a_cone_inside_the_dripper'},
-            {'name': 'Place_the_dripper_on_top_of_the_mug'},
+            {'name': 'Turn_on_kitchen_scale'},
+            {'name': 'Place_bowl_on_scale'},
+            {'name': 'Zero_scale'},
+            {'name': 'Add_coffee_beans_until_scale_reads_25_grams'},
+            {'name': 'Pour_coffee_beans_into_coffee_grinder'},
             {'name': 'Done'}
         ]
 
@@ -112,8 +113,8 @@ class CoffeeDemoTask():
         # For more info, see:
         # https://github.com/pytransitions/transitions#automatic-transitions-for-all-states
         self.to_state_dict = {
-            'Pour_12oz_of_water_into_a_liquid_measuring_cup':
-                self.to_Pour_12oz_of_water_into_a_liquid_measuring_cup,
+            'Pour_12_ounces_of_water_into_liquid_measuring_cup':
+                self.to_Pour_12_ounces_of_water_into_liquid_measuring_cup,
             'Pour_water_from_the_liquid_measuring_cup_into_the_electric_kettle':
                 self.to_Pour_water_from_the_liquid_measuring_cup_into_the_electric_kettle,
             'Turn_on_the_kettle':
@@ -190,8 +191,8 @@ class TaskMonitor(Node):
         # function. The keys of this dictionary must match the activity
         # detector's output.
         self._activity_action_dict = {
-            'Pour 12oz of water into a liquid measuring cup':
-                self._task.Pour_12oz_of_water_into_a_liquid_measuring_cup,
+            'Pour 12 ounces of water into liquid measuring cup':
+                self._task.Pour_12_ounces_of_water_into_liquid_measuring_cup,
             'Pour water from the liquid measuring cup into the electric kettle':
                 self._task.Pour_water_from_the_liquid_measuring_cup_into_the_electric_kettle,
             'Turn on the kettle':
