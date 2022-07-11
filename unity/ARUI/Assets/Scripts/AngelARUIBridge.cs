0f1ffd6ce2501d5eee0374a8e7a39e931a951a73
Josh Anderson <josh.anderson@kitware.com>
2022-07-11 11:35:49 -0500

    Use step index to call Setcurrenttask

--- unity/ARUI/Assets/Scripts/AngelARUIBridge.cs
+++ unity/ARUI/Assets/Scripts/AngelARUIBridge.cs
@@ -17,27 +17,22 @@ public class AngelARUIBridge : MonoBehaviour
 
     private Logger _logger = null;
 
-    // TODO: this should be replace with the results of the QueryTaskGraph call
+    // TODO: this should be replaced with the results of the QueryTaskGraph call
     private string[,] tasks =
     {
-        {"0", "Boil 12 ounces of water"},
-        {"1", "Measure 12 ounces of water in the liquid measuring cup"},
-        {"1", "Pour the water from the liquid measuring cup into  the electric kettle"},
-        {"1", "Turn on the electric kettle by pushing the button underneath the handle"},
-        {"1", "Boil the water. The water is done boiling when the button underneath the handle pops up"},
-
-        {"0","Assemble the filter cone"},
-        {"1", "While the water is boiling, assemble the filter cone. Place the dripper on top of a coffee mug"},
-        {"1", "Prepare the filter insert by folding the paper filter in half to create a semi-circle, and in half again to create a quarter-circle. Place the paper filter in the dripper and spread open to create a cone."},
-        {"1", "Take the coffee filter and fold it in half to create a semi-circle"},
-        {"1", "Folder the filter in half again to create a quarter-circle"},
-        {"1", "Place the folded filter into the dripper such that the the point of the quarter-circle rests in the center of the dripper"},
-        {"1", "Spread the filter open to create a cone inside the dripper"},
-        {"1", "Place the dripper on top of the mug"},
-
-        {"0","Ground the coffee and add to the filter cone"},
-        {"1","Weigh the coffee beans and grind until the coffee grounds are the consistency of coarse sand, about 20 seconds. Transfer the grounds to the filter cone."},
-        {"1","Turn on the kitchen scale"},
+        {"0", "Pour 12 ounces of water into liquid measuring cup"},
+        {"0", "Pour the water from the liquid measuring cup into the electric kettle"},
+        {"0", "Turn on the kettle"},
+        {"0", "Place the dripper on top of the mug"},
+        {"0", "Take the coffee filter and fold it in half to create a semi circle"},
+        {"0", "Fold the filter in half again to create a quarter circle"},
+        {"0", "Place the folded filter into the dripper such that the point of the quarter circle rests in the center of the dripper"},
+        {"0", "Spread the filter open to create a cone inside the dripper"},
+        {"0", "Turn on kitchen scale"},
+        {"0", "Place bowl on scale"},
+        {"0", "Zero scale"},
+        {"0", "Add coffee beans until scale reads 25 grams"},
+        {"0", "Pour coffee beans into coffee grinder"}
     };
 
     /// <summary>
@@ -69,15 +64,15 @@ public class AngelARUIBridge : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Callback function for the AruiUpdate subscriber.
+    /// Updates the AngelARUI instance with the latest info.
+    /// </summary>
+    /// <param name="msg"></param>
     private void AruiUpdateCallback(AruiUpdateMsg msg)
     {
         Logger log = logger();
-        log.LogInfo("Got update: " + msg.current_task_uid);
 
-        // Call ARUI set task
-
-        // TODO - need to map the UID to the step index
-        //AngelARUI.Instance.SetCurrentTaskID();
-
+        AngelARUI.Instance.SetCurrentTaskID(msg.task_update.current_step_id);
     }
 }
