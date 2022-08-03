c135bb27fa4fdb743c9ae34b738e98877364993e
Xiao Yu <jasonyux17@gmail.com>
2022-08-02 23:23:49 -0400

    added eye gaze encoder interface

--- angel_system/interfaces/emotion_models.py
+++ angel_system/interfaces/emotion_models.py
@@ -37,3 +37,39 @@ class SpeechEmotionModel (Configurable, Pluggable):
         Calls `classify_emotion() with the given iterable modality representation.`
         """
         return self.classify(X, sampling_rate)
+
+
+class EyeGazeEncoderModel(Configurable, Pluggable):
+    """
+    Model to extract features given a sequence of eye gaze data
+    takes in input of shape (6, SEQ_LEN) ``numpy.ndarray`` type arrays, consisting of ((gaze_x, gaze_y, gaze_z, head_x, head_y, head_z), time)
+    """
+    @abc.abstractmethod
+    def __init__(self, model):
+        """
+        model: a serialized model. __init__ should implement loading model either using pickle or torch.
+        """
+
+
+    @abc.abstractmethod
+    def encode(
+      self,
+      X: np.ndarray,
+      sampling_rate: int
+    ) -> int:
+        """
+        Encode the given eye gaze data into a feature vector
+        :param X: Iterable of some input modality as numpy arrays.
+        :param sampling_rate: sampling rate to undersample the input X. used to accord to the model's input SEQ_LEN
+        :return: encoded feature vector
+        """
+
+    def __call__(
+      self,
+      X: Iterable[np.ndarray],
+      sampling_rate: int
+    ) -> int:
+        """
+        Calls `encode()` with the given eye gaze input
+        """
+        return self.encode(X, sampling_rate)
