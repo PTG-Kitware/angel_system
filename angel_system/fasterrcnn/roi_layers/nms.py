d60a9f0cb4970f59b9142dd97c5063e4edb0d4bb
Josh Anderson <josh.anderson@kitware.com>
2022-09-23 15:41:57 -0500

    Add fasterrcnn subfolder

---
+++ angel_system/fasterrcnn/roi_layers/nms.py
@@ -0,0 +1,8 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+# from ._utils import _C
+#from angel_system.fasterrcnn import _C
+import torchvision
+
+nms = torchvision.ops.nms
+# nms.__doc__ = """
+# This function performs Non-maximum suppresion"""
