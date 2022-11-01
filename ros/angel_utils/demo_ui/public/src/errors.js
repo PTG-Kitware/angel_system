9edf16de27838ae26484f842cdc9ed43b8a9a814
Hannah DeFazio <hannah.defazio@kitware.com>
2022-11-01 16:33:41 -0400

    Remove collapsing feature on errors

--- ros/angel_utils/demo_ui/public/src/errors.js
+++ ros/angel_utils/demo_ui/public/src/errors.js
@@ -37,11 +37,6 @@ $.get( "/ns")
             btn.addEventListener("click", function() {
                 this.classList.toggle("active");
                 var content = this.nextElementSibling;
-                if (content.style.display === "block") {
-                    content.style.display = "none";
-                } else {
-                    content.style.display = "block";
-                }
             }); 
         }
     });
