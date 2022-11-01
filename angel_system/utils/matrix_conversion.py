0727bdc7f25700e96fb52ea12cf4a4fe77daf8b2
Josh Anderson <josh.anderson@kitware.com>
2022-11-01 15:14:52 -0500

    Cleanup and add some comments

--- angel_system/utils/matrix_conversion.py
+++ angel_system/utils/matrix_conversion.py
@@ -1,59 +1,69 @@
+from array import array
+from typing import List
+
 import numpy as np
+import numpy.typing as npt
 
-def convert_1d_4x4_to_2d_matrix(matrix_1d):
-    matrix_2d = [[], [], [], []]
-    for row in range(4):
-        for col in range(4):
-            idx = row * 4 + col
-            matrix_2d[row].append(matrix_1d[idx])
+# NOTE: These values were extracted from the projection matrix provided by the
+# Unity main camera with the Windows MR plugin (now deprecated).
+# For some unknown reason, the values provided by the Open XR plugin are
+# different and do not provide the correct results. In the future, we should
+# figure out why they are different or extract the focal length values from the
+# MediaFrameReader which provides the frames, instead of the Unity main camera.
+FOCAL_LENGTH_X = 1.6304
+FOCAL_LENGTH_Y = 2.5084
 
-    return matrix_2d
+PROJECTION_MATRIX = np.asarray([
+    [FOCAL_LENGTH_X, 0.0, 0.0, 0.0],
+    [0.0, FOCAL_LENGTH_Y, 0.0, 0.0],
+    [0.0, 0.0, -1.0020020008087158, -0.20020020008087158],
+    [0.0, 0.0, -1.0, 0.0]
+])
+
+
+def convert_1d_4x4_to_2d_matrix(matrix_1d: array) -> npt.NDArray:
+    """
+    Convert 1D array of length 16 to 2D 4x4 matrix.
+
+    :param matrix_1d: 1D array of length 16.
+
+    :returns: A 2D array of shape 4x4.
+    """
+    return np.asarray(matrix_1d).reshape(4, 4)
 
 
 def project_3d_pos_to_2d_image(
-    position,
-    inverse_world_mat,
-    projection_mat
-):
+    position: npt.NDArray,
+    inverse_world_mat: npt.NDArray,
+    projection_mat: npt.NDArray,
+) -> List[float]:
     """
     Projects the 3d position vector into 2d image space using the given world
     to camera and camera projection matrices.
 
     The image coordinates returned are in the range [-1, 1]. Values outside of
     this range are clipped.
+
+    :param position: 1D array representing the 3D position of the object in
+        world space.
+    :param inverse_world_mat: 2D matrix that is the inverse of the
+        camera-to-world matrix (a.k.a the world-to-camera matrix). This is
+        provided by the HoloLens RGB camera for each frame.
+    :param projection_mat: 2D matrix to use to convert from camera space to
+        image space. This is provided by the HoloLens. This is provided by
+        the HoloLens RGB camera for each frame.
+
+    :returns: The points on the 2D image plane as a list of [x, y, 1.0]
     """
     # Convert from world space to camera space
     x = np.matmul(inverse_world_mat, position)
     # Convert from camera space to image space
     image = np.matmul(projection_mat, x)
-    #print("image coords", image)
 
     # Normalize
     image_scaled = image / image[3]
-    #print("image scaled coords", image_scaled)
 
     image_scaled_x = image_scaled[0]
     image_scaled_y = image_scaled[1]
-
-    '''
-
-    # Limit to -1 and 1
-    if image_scaled_x > 1:
-        image_scaled_x = 1
-    elif image_scaled_x < -1:
-        image_scaled_x = -1
-    if image_scaled_y > 1:
-        image_scaled_y = 1
-    elif image_scaled_y < -1:
-        image_scaled_y = -1
-    '''
-
-
-    '''
-    # Convert to screen coordinates
-    projected_point_x = image_scaled[0] * half_width + half_width
-    projected_point_y = image_scaled[1] * half_height + half_height
-    #print("pixel coords", projected_point_x, projected_point_y)
-    '''
 
     return [image_scaled_x, image_scaled_y, 1.0]
