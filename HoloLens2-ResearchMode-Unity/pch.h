63928e13eceebef3742c637b61a2888e01ec105b
Josh Anderson <josh.anderson@kitware.com>
2023-02-16 13:02:55 -0600

    Remove old HL2 research mode plugin

--- HoloLens2-ResearchMode-Unity/pch.h
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-#pragma once
-#include <unknwn.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Foundation.Numerics.h>
-#include "ResearchModeApi.h"
