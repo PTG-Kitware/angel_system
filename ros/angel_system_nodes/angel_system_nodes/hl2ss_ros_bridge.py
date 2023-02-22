ed7f5489282b859c88411ded512566707f01a1cb
Josh Anderson <josh.anderson@kitware.com>
2023-02-22 14:40:04 -0600

    Add audio streaming

--- ros/angel_system_nodes/angel_system_nodes/hl2ss_ros_bridge.py
+++ ros/angel_system_nodes/angel_system_nodes/hl2ss_ros_bridge.py
@@ -10,6 +10,7 @@ from geometry_msgs.msg import (
     Pose,
     Quaternion,
 )
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -17,6 +18,7 @@ from sensor_msgs.msg import Image
 from angel_msgs.msg import (
     HandJointPose,
     HandJointPosesUpdate,
+    HeadsetAudioData,
 )
 from angel_system.hl2ss_viewer import hl2ss
 from angel_utils.conversion import hl2ss_stamp_to_ros_time
@@ -62,6 +64,11 @@ class HL2SSROSBridge(Node):
             .get_parameter_value()
             .string_value
         )
+        self._audio_topic = (
+            self.declare_parameter("audio_topic", "")
+            .get_parameter_value()
+            .string_value
+        )
         self.ip_addr = (
             self.declare_parameter("ip_addr", "")
             .get_parameter_value()
@@ -88,6 +95,8 @@ class HL2SSROSBridge(Node):
             raise ValueError("Please provide the image topic with the `image_topic` parameter")
         if self._hand_pose_topic == "":
             raise ValueError("Please provide the hand pose topic with the `hand_pose_topic` parameter")
+        if self._audio_topic == "":
+            raise ValueError("Please provide the audio topic with the `audio_topic` parameter")
         if self.ip_addr == "":
             raise ValueError("Please provide HL2 IPv4 address with the `ip_addr` parameter")
 
@@ -99,17 +108,24 @@ class HL2SSROSBridge(Node):
         # Define HL2SS server ports
         self.pv_port = hl2ss.StreamPort.PERSONAL_VIDEO
         self.si_port = hl2ss.StreamPort.SPATIAL_INPUT
+        self.audio_port = hl2ss.StreamPort.MICROPHONE
 
         # Create frame publisher
-        self.frame_publisher = self.create_publisher(
+        self.ros_frame_publisher = self.create_publisher(
             Image,
             self._image_topic,
             1
         )
         # Create the hand joint pose publisher
-        self.hand_publisher = self.create_publisher(
+        self.ros_hand_publisher = self.create_publisher(
             HandJointPosesUpdate,
             self._hand_pose_topic,
+            1
+        )
+        # Create the audio publisher
+        self.ros_audio_publisher = self.create_publisher(
+            HeadsetAudioData,
+            self._audio_topic,
             1
         )
 
@@ -118,6 +134,8 @@ class HL2SSROSBridge(Node):
         log.info("PV client connected!")
         self.connect_hl2ss_si()
         log.info("SI client connected!")
+        self.connect_hl2ss_audio()
+        log.info("Audio client connected!")
 
         log.info("Starting publishing threads...")
         # Start the frame publishing thread
@@ -125,7 +143,7 @@ class HL2SSROSBridge(Node):
         self._pv_active.set()
         self._pv_thread = Thread(
             target=self.pv_publisher,
-            name="publish_frames"
+            name="publish_pv"
         )
         self._pv_thread.daemon = True
         self._pv_thread.start()
@@ -134,10 +152,19 @@ class HL2SSROSBridge(Node):
         self._si_active.set()
         self._si_thread = Thread(
             target=self.si_publisher,
-            name="publish_frames"
+            name="publish_si"
         )
         self._si_thread.daemon = True
         self._si_thread.start()
+        # Start the audio data thread
+        self._audio_active = Event()
+        self._audio_active.set()
+        self._audio_thread = Thread(
+            target=self.audio_publisher,
+            name="publish_audio"
+        )
+        self._audio_thread.daemon = True
+        self._audio_thread.start()
         log.info("Starting publishing threads... Done")
 
     def connect_hl2ss_pv(self) -> None:
@@ -182,6 +209,19 @@ class HL2SSROSBridge(Node):
         )
         self.hl2ss_si_client.open()
 
+    def connect_hl2ss_audio(self) -> None:
+        """
+        Creates the HL2SS audio client and connects it to the
+        server on the headset.
+        """
+        # AAC 24000 bytes/s per channel
+        profile = hl2ss.AudioProfile.AAC_24000
+
+        self.hl2ss_audio_client = hl2ss.rx_decoded_microphone(
+            self.ip_addr, self.audio_port, hl2ss.ChunkSize.MICROPHONE, profile
+        )
+        self.hl2ss_audio_client.open()
+
     def shutdown_clients(self) -> None:
         """
         Shuts down the frame publishing thread and the HL2SS client.
@@ -192,9 +232,14 @@ class HL2SSROSBridge(Node):
         self.get_logger().info("PV thread closed")
 
         # Stop SI publishing thread
-        self._si_active.clear()  # make RT active flag "False"
+        self._si_active.clear()
         self._si_thread.join()
         self.get_logger().info("SI thread closed")
+
+        # Stop audio publishing thread
+        self._audio_active.clear()
+        self._audio_thread.join()
+        self.get_logger().info("Audio thread closed")
 
         # Close client connections
         self.hl2ss_pv_client.close()
@@ -203,6 +248,9 @@ class HL2SSROSBridge(Node):
 
         self.hl2ss_si_client.close()
         self.get_logger().info("SI client disconnected")
+
+        self.hl2ss_audio_client.close()
+        self.get_logger().info("Audio client disconnected")
 
     def pv_publisher(self) -> None:
         """
@@ -215,11 +263,11 @@ class HL2SSROSBridge(Node):
             try:
                 image_msg = BRIDGE.cv2_to_imgmsg(data.payload, encoding="bgr8")
                 image_msg.header.stamp = hl2ss_stamp_to_ros_time(data.timestamp)
-                image_msg.header.frame_id = "PVFramesRGB"
+                image_msg.header.frame_id = "PVFramesBGR"
             except TypeError as e:
                 self.get_logger().warning(f"{e}")
 
-            self.frame_publisher.publish(image_msg)
+            self.ros_frame_publisher.publish(image_msg)
 
     def si_publisher(self) -> None:
         """
@@ -238,12 +286,41 @@ class HL2SSROSBridge(Node):
                 hand_msg_left = self.create_hand_pose_msg_from_si_data(
                     si_data, "Left", data.timestamp
                 )
-                self.hand_publisher.publish(hand_msg_left)
+                self.ros_hand_publisher.publish(hand_msg_left)
             if si_data.is_valid_hand_right():
                 hand_msg_right = self.create_hand_pose_msg_from_si_data(
                     si_data, "Right", data.timestamp
                 )
-                self.hand_publisher.publish(hand_msg_right)
+                self.ros_hand_publisher.publish(hand_msg_right)
+
+    def audio_publisher(self) -> None:
+        """
+        Thread the gets audio packets from the HL2SS audio client, converts
+        the data to ROS HeadsetAudioData messages, and publishes them.
+        """
+        while self._audio_active.wait(0):  # will quickly return false if cleared.
+            data = self.hl2ss_audio_client.get_next_packet()
+
+            n_channels, sample_len = data.payload.shape
+            assert n_channels == hl2ss.Parameters_MICROPHONE.CHANNELS
+
+            audio = np.zeros((data.payload.size), dtype=data.payload.dtype)
+            audio[0::2] = data.payload[0, :]
+            audio[1::2] = data.payload[1, :]
+
+            sample_rate = hl2ss.Parameters_MICROPHONE.SAMPLE_RATE
+            sample_duration = (1.0 / sample_rate) * sample_len
+
+            audio_msg = HeadsetAudioData()
+            audio_msg.header.stamp = hl2ss_stamp_to_ros_time(data.timestamp)
+            audio_msg.header.frame_id = "AudioData"
+
+            audio_msg.channels = n_channels
+            audio_msg.sample_rate = sample_rate
+            audio_msg.sample_duration = sample_duration
+            audio_msg.data = audio.tolist()
+
+            self.ros_audio_publisher.publish(audio_msg)
 
     def create_hand_pose_msg_from_si_data(
         self,
