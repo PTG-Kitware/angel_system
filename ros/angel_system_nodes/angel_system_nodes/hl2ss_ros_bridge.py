81b010bf186f8010fc65628797fb3708d5ed7916
Paul Tunison <paul.tunison@kitware.com>
2023-05-03 13:24:47 -0400

    Deduplicate common hand joint label list

--- ros/angel_system_nodes/angel_system_nodes/hl2ss_ros_bridge.py
+++ ros/angel_system_nodes/angel_system_nodes/hl2ss_ros_bridge.py
@@ -28,6 +28,7 @@ from angel_msgs.msg import (
 )
 from angel_system.hl2ss_viewer import hl2ss
 from angel_utils import declare_and_get_parameters, RateTracker
+from angel_utils.hand import JOINT_LIST
 
 
 BRIDGE = CvBridge()
@@ -36,43 +37,6 @@ BRIDGE = CvBridge()
 # Must be > 0
 # Value copied from hl2ss/viewer/cient_pv.py example
 PV_BITRATE = 5*1024*1024
-
-# List containing joint names that matches the ordering in the HL2SS
-# SI_HandJointKind class. Names semantically match the output from the MRTK API
-# though the ordering of the joins is matching that of the windows perception
-# API.
-# MRTK API: https://learn.microsoft.com/en-us/dotnet/api/microsoft.mixedreality.toolkit.utilities.trackedhandjoint?preserve-view=true&view=mixed-reality-toolkit-unity-2020-dotnet-2.8.0
-# Windows Perception API: https://learn.microsoft.com/en-us/uwp/api/windows.perception.people.handjointkind?view=winrt-22621
-# Matching the names of the MRTK API for downstream components to continue to
-# match against.
-JOINT_LIST = [
-    "Palm",
-    "Wrist",
-    "ThumbMetacarpalJoint",
-    "ThumbProximalJoint",
-    "ThumbDistalJoint",
-    "ThumbTip",
-    "IndexMetacarpal",
-    "IndexKnuckle",
-    "IndexMiddleJoint",
-    "IndexDistalJoint",
-    "IndexTip",
-    "MiddleMetacarpal",
-    "MiddleKnuckle",
-    "MiddleMiddleJoint",
-    "MiddleDistalJoint",
-    "MiddleTip",
-    "RingMetacarpal",
-    "RingKnuckle",
-    "RingMiddleJoint",
-    "RingDistalJoint",
-    "RingTip",
-    "PinkyMetacarpal",
-    "PinkyKnuckle",
-    "PinkyMiddleJoint",
-    "PinkyDistalJoint",
-    "PinkyTip",
-]
 
 PARAM_PV_IMAGES_TOPIC = "image_topic"
 PARAM_HAND_POSE_TOPIC = "hand_pose_topic"
