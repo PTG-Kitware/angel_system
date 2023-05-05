5910c7d041e05fc5a2e06c5c29c3c78b7bb8efa7
Hannah DeFazio <hannah.defazio@kitware.com>
2023-05-05 11:01:41 -0400

    Remove comment

--- angel_system/berkeley/detectron2/modeling/roi_heads/roi_heads.py
+++ angel_system/berkeley/detectron2/modeling/roi_heads/roi_heads.py
@@ -528,7 +528,6 @@ class Res5ROIHeads(ROIHeads):
 
 @ROI_HEADS_REGISTRY.register()
 class StandardROIHeads_PLUS_CONTACT(ROIHeads):
-#class StandardROIHeads(ROIHeads):
     """
     It's "standard" in a sense that there is no ROI transform sharing
     or feature sharing between tasks.
@@ -1019,7 +1018,6 @@ class StandardROIHeads_PLUS_CONTACT(ROIHeads):
 
 
 @ROI_HEADS_REGISTRY.register()
-#class StandardROIHeads_PLUS_CONTACT(ROIHeads):
 class StandardROIHeads(ROIHeads):
     """
     It's "standard" in a sense that there is no ROI transform sharing
