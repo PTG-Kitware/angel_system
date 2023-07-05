b7c159e08014353df8b6195cd19febdacd9b706d
Derek Ahmed <derekhmd@gmail.com>
2023-07-05 16:36:48 -0400

    Refactor GptEmotionDetector to inherit from BaseEmotionDetector

--- ros/angel_system_nodes/angel_system_nodes/base_emotion_detector.py
+++ ros/angel_system_nodes/angel_system_nodes/base_emotion_detector.py
@@ -12,15 +12,10 @@ IN_EXPECT_USER_INTENT_TOPIC = "expect_user_intent_topic"
 IN_INTERP_USER_INTENT_TOPIC = "interp_user_intent_topic"
 OUT_INTERP_USER_EMOTION_TOPIC = "user_emotion_topic"
 
-# Model implementation configurations. As of 2023Q2, only "vader" mode is
-# supported. This is tied with the VaderSentiment
-# (https://github.com/cjhutto/vaderSentiment).
-EMOTION_DETECTOR_MODE = "model_mode"
-
 # Currently supported emotions. This is tied with the emotions
 # output to VaderSentiment (https://github.com/cjhutto/vaderSentiment) and
 # will be subject to change in future iterations.
-VADER_SENTIMENT_LABEL_MAPPINGS = {
+LABEL_MAPPINGS = {
     "pos": "positive",
     "neg": "negative",
     "neu": "neutral"
@@ -29,7 +24,7 @@ VADER_SENTIMENT_LABEL_MAPPINGS = {
 VADER_NEGATIVE_COMPOUND_THRESHOLD = -0.05
 VADER_POSITIVE_COMPOUND_THRESHOLD = 0.05
 
-class EmotionDetector(Node):
+class BaseEmotionDetector(Node):
     '''
     As of Q22023, emotion detection is derived via VaderSentiment
     (https://github.com/cjhutto/vaderSentiment).
@@ -42,8 +37,7 @@ class EmotionDetector(Node):
         parameter_names = [
             IN_EXPECT_USER_INTENT_TOPIC,
             IN_INTERP_USER_INTENT_TOPIC,
-            OUT_INTERP_USER_EMOTION_TOPIC,
-            EMOTION_DETECTOR_MODE
+            OUT_INTERP_USER_EMOTION_TOPIC
         ]
         set_parameters = self.declare_parameters(
             namespace="",
@@ -60,8 +54,6 @@ class EmotionDetector(Node):
         self._in_interp_uintent_topic = self.get_parameter(IN_INTERP_USER_INTENT_TOPIC).value
         self._in_expect_uintent_topic = self.get_parameter(IN_EXPECT_USER_INTENT_TOPIC).value
         self._out_interp_uemotion_topic = self.get_parameter(OUT_INTERP_USER_EMOTION_TOPIC).value
-        self.mode = \
-            self.get_parameter(EMOTION_DETECTOR_MODE).value
         self.log.info(f"Interpreted User Intents topic: "
                       f"({type(self._in_interp_uintent_topic).__name__}) "
                       f"{self._in_interp_uintent_topic}")
@@ -71,9 +63,6 @@ class EmotionDetector(Node):
         self.log.info(f"Interpreted User Emotion topic: "
                       f"({type(self._out_interp_uemotion_topic).__name__}) "
                       f"{self._out_interp_uemotion_topic}")
-        self.log.info(f"Model mode: "
-                      f"({type(self.mode).__name__}) "
-                      f"{self.mode}")
         
         # Handle subscription/publication topics.
         self.interp_uintent_subscription = self.create_subscription(
@@ -81,7 +70,6 @@ class EmotionDetector(Node):
             self._in_interp_uintent_topic,
             self.listener_callback,
             1)
-        
         self.expect_uintent_subscription = self.create_subscription(
             InterpretedAudioUserIntent,
             self._in_expect_uintent_topic,
@@ -96,12 +84,7 @@ class EmotionDetector(Node):
         self.handler_thread = threading.Thread(target=self.process_message_queue)
         self.handler_thread.start()
 
-        # Add other model modes here in the below conditional cases as they
-        # get implemented. As of 2023Q2, only Vader is supported.
-        if self.mode == "vader":
-            self.sentiment_analysis_model = SentimentIntensityAnalyzer()
-        else:
-            raise ValueError("Invalid mode provided")
+        self.sentiment_analysis_model = SentimentIntensityAnalyzer()
 
     def _get_vader_sentiment_analysis(self, utterance: str):
         '''
@@ -111,11 +94,11 @@ class EmotionDetector(Node):
         polarity_scores = \
             self.sentiment_analysis_model.polarity_scores(utterance)
         if polarity_scores['compound'] >= VADER_POSITIVE_COMPOUND_THRESHOLD:
-            classification = VADER_SENTIMENT_LABEL_MAPPINGS['pos']
+            classification = LABEL_MAPPINGS['pos']
         elif  polarity_scores['compound'] <= VADER_NEGATIVE_COMPOUND_THRESHOLD:
-            classification = VADER_SENTIMENT_LABEL_MAPPINGS['neg']
+            classification = LABEL_MAPPINGS['neg']
         else:
-            classification = VADER_SENTIMENT_LABEL_MAPPINGS['neu']
+            classification = LABEL_MAPPINGS['neu']
 
         confidence = 1.00
         colored_utterance = colored_utterance = colored(utterance, "light_blue")
@@ -126,15 +109,12 @@ class EmotionDetector(Node):
                       f"and score={confidence}")     
         return (classification, confidence)
 
-    def get_inference(self, utterance: str):
+    def get_inference(self, msg):
         '''
         Abstract away the different model inference calls depending on the
         node's configure model mode.
         '''
-        if self.mode == "vader":
-            return self._get_vader_sentiment_analysis(utterance)
-        raise ValueError("Invalid Emotion Detector has been configured." +\
-                          f"{self.mode} is an invalid mode.")       
+        return self._get_vader_sentiment_analysis(msg.utterance_text)
 
     def listener_callback(self, msg):
         '''
@@ -153,16 +133,23 @@ class EmotionDetector(Node):
         while True:
             msg = self.message_queue.get()
             self.log.debug(f"Processing message:\n\n\"{msg.utterance_text}\"")
-            classification, confidence_score  = self.get_inference(msg.utterance_text)
+            classification, confidence_score  = self.get_inference(msg)
             self.publish_detected_emotion(msg.utterance_text, classification, confidence_score)
 
     def publish_detected_emotion(self, utterance: str,  classification: str,
                                   confidence_score: float):
+        '''
+        Handles message publishing for an utterance with a detected emotion classification.
+        '''
         emotion_msg = InterpretedAudioUserEmotion()
         emotion_msg.utterance_text = utterance
         emotion_msg.user_emotion = classification
-        emotion_msg.confidence = confidence_score   
+        emotion_msg.confidence = confidence_score
         self._interp_emo_publisher.publish(emotion_msg)
+        colored_utterance = colored(utterance, "light_blue")
+        colored_emotion = colored(classification, "light_green")
+        self.log.info(f"Publishing {{\"{colored_emotion}\": {confidence_score}}} " +\
+                      f"to {self._out_interp_uemotion_topic} for:\n>>> \"{colored_utterance}\"")
 
     def _apply_filter(self, msg):
         '''
@@ -178,7 +165,7 @@ class EmotionDetector(Node):
 
 def main():
     rclpy.init()
-    emotion_detector = EmotionDetector()
+    emotion_detector = BaseEmotionDetector()
     rclpy.spin(emotion_detector)
     emotion_detector.destroy_node()
     rclpy.shutdown()
