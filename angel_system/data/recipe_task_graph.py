6b8667519020b8281aeb2e0cf47afdc1b6150ef8
Hannah DeFazio <hannah.defazio@kitware.com>
2023-07-10 17:03:49 -0400

    Bug fixes, docs strings

--- angel_system/data/recipe_task_graph.py
+++ angel_system/data/recipe_task_graph.py
@@ -1,18 +1,35 @@
 import os
 import yaml
 import random
+import warnings
 import argparse
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
+from pathlib import Path
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+
+dir_path = Path(__file__).parent
 
 
 def load_recipe(recipe):
+    """
+    Loads a recipe action configuration file
+
+    :param recipe: String representing the name of the recipe to run
+
+    :return: A tuple consisting of two elements:
+        0: String containing the full title of the recipe
+        1: List of activity labels
+    """
     config_dir = f"{dir_path}/../../config/activity_labels"
+    assert os.path.isdir(
+        config_dir
+    ), f"Configuration directory {config_dir} does not exist"
+
     config_fname = f"{config_dir}/recipe_{recipe}.yaml"
+    assert os.path.exists(config_fname), f"Configuration {config_fname} does not exist"
 
     with open(config_fname, "r") as stream:
         config = yaml.safe_load(stream)
@@ -30,59 +47,84 @@ def load_recipe(recipe):
 
 
 def get_all_recipe_orders(labels):
+    """
+    Creates a recipe graph and determines all
+    possible topological sortings of the graph
+
+    :param labels: List of activity labels
+
+    :return: A tuple consisting of two elements:
+        0: List of all possible topological sortings of the recipe graph
+        1: List of ids that represent the root node of a set of repeated actions
+    """
     G = nx.DiGraph()
+    repeated_ids = []
 
     for action in labels:
-        act_label = action["label"]
-        depends = action["depends"]
+        act_id = action["id"]
+        G.add_node(act_id)
+
+        if act_id == 0:
+            continue
+
+        depends = action.get("depends", None)
 
         if "repeat" in action.keys():
             repeat = action["repeat"]
+            repeated_ids.append(act_id)
 
-            # Node that indicated when the full repetition is finished
-            all_act_label = f"{act_label}-all"
-            G.add_node(all_act_label)
             for r in range(repeat):
-                new_act_label = f"{act_label}-{r}"
-                G.add_edge(new_act_label, all_act_label)
+                new_act_id = f"{act_id}-{r}"
+                G.add_edge(new_act_id, act_id)
 
-                G.add_node(new_act_label, root_label=act_label)
+                G.add_node(new_act_id, root_label=act_id)
                 if depends is not None:
                     for d in depends:
-                        G.add_edge(d, new_act_label)
+                        G.add_edge(d, new_act_id)
+                else:
+                    G.add_edge(0, new_act_id)
         else:
-            G.add_node(act_label)
-
             if depends is not None:
                 for d in depends:
-                    G.add_edge(d, act_label)
+                    G.add_edge(d, act_id)
+            else:
+                G.add_edge(0, act_id)
+
+    nx.draw(G, with_labels=True)
+    plt.savefig("coffee.png")
 
     all_possible_orders = list(nx.all_topological_sorts(G))
-    return all_possible_orders
+    return all_possible_orders, repeated_ids
 
 
-def print_recipe_order(recipe_title, labels, all_possible_orders):
+def print_recipe_order(recipe_title, labels, all_possible_orders, repeated_ids):
+    """
+    Prints a list of instructions for a recipe
+
+    :param recipe_title: String containing the full title of the recipe
+    :param labels: List of activity labels
+    :param all_possible_orders: List of all possible topological sortings of the recipe graph
+    :param repeated_ids: List of ids that represent the root node of a set of repeated actions.
+        These ids are skipped when printing the recipe steps to avoid duplication of instructions
+    """
     # TODO: create sample bias towards regular order?
     recipe_order = random.choice(all_possible_orders)
 
     print(f"\n{recipe_title}")
     print("=" * len(recipe_title))
-
     recipe_actions = [
-        al
-        for al in recipe_order
-        if not al.endswith("-all") and al not in ["start", "done"]
+        al for al in recipe_order if al not in [1, labels[-1]["id"]] + repeated_ids
     ]
-    for i, action_label in enumerate(recipe_actions):
-        try:
-            action = [l for l in labels if l["label"] == action_label][0]
-        except:
+    for i, action_id in enumerate(recipe_actions):
+        if type(action_id) is int:
+            action = [l for l in labels if l["id"] == action_id][0]
+        elif "-" in str(action_id):
             # The label might have a repeat index attached to it
-            action = [
-                l
-                for l in labels
-                if l["label"] == "-".join(action_label.split("-")[:-1])
-            ][0]
+            action = [l for l in labels if l["id"] == int(action_id.split("-")[0])][0]
+        else:
+            warnings.warn(f"Unknown action id: {action_id}")
+            continue
+
         str_ = action["full_str"]
 
         print(f"{i+1}: {str_}")
@@ -112,8 +154,8 @@ def main():
 
     recipe_title, labels = load_recipe(args.recipe)
 
-    all_possible_orders = get_all_recipe_orders(labels)
-    print_recipe_order(recipe_title, labels, all_possible_orders)
+    all_possible_orders, repeated_ids = get_all_recipe_orders(labels)
+    print_recipe_order(recipe_title, labels, all_possible_orders, repeated_ids)
 
 
 if __name__ == "__main__":
