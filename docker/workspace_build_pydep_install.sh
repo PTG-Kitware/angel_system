e270417daf0c0ae01cae5f601261c96609ff7373
Paul Tunison <paul.tunison@kitware.com>
2023-09-19 15:47:09 -0400

    Add TCN-HPL submodule, include into container env
    This caused some chaos. Environment has been moved to use poetry
    directly and treat `${HOME}/.local` as a "venv" for such purposes,
    "enabled" by default via the setting of key environment variables.

--- docker/workspace_build_pydep_install.sh
+++ docker/workspace_build_pydep_install.sh
@@ -7,11 +7,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # `poetry.lock` and `pyproject.toml` files.
 cd "$SCRIPT_DIR"
 
-# Install requirements ONLY as translated into frozen requirements form and
-# then installed entirely into user space (`~/.local/`).
-poetry export --dev -f requirements.txt |
-  pip3 install --no-deps --user -r /dev/stdin
-
-# Poetry extraneously finds matplotlib requiring setuptools-scm.
-# theoretically being "fixed" in matplotlib 3.6
-pip uninstall -y setuptools-scm
+poetry install
