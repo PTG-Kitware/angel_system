1d4501be117cff22c7662566a32b5a653220ed04
Josh Anderson <josh.anderson@kitware.com>
2023-10-25 12:30:48 -0500

    Update feedback generator to listen to SystemTestResponse topic

--- ros/angel_system_nodes/angel_system_nodes/feedback_generator.py
+++ ros/angel_system_nodes/angel_system_nodes/feedback_generator.py
@@ -9,11 +9,14 @@ from angel_msgs.msg import (
     ActivityDetection,
     AruiObject3d,
     AruiUpdate,
+    AruiUserNotification,
     InterpretedAudioUserIntent,
     ObjectDetection3dSet,
+    SystemTextResponse,
     TaskUpdate,
     VisionBoundingBox3d,
 )
+from angel_utils import declare_and_get_parameters
 
 
 # Parameter name constants
@@ -22,83 +25,43 @@ PARAM_OBJECT_DET_TOPIC = "object_detection_topic"
 PARAM_TASK_MONITOR_TOPIC = "task_monitor_topic"
 PARAM_ARUI_UPDATE_TOPIC = "arui_update_topic"
 PARAM_INTERP_USER_INTENT_TOPIC = "interp_user_intent_topic"
+PARAM_SYSTEM_TEXT_RESPONSE_TOPIC = "system_text_response_topic"
 
 
 class FeedbackGenerator(Node):
     """
-    ROS node responsible for sending activity, detection, and task information to the ARUI when the information has updated.
+    ROS node responsible for sending activity, detection, and task information
+    to the ARUI when the information has updated.
 
-    Takes in information from the `angel_msgs/ActivityDetection`, `angel_msgs/ObjectDetection3dSet`, and `angel_msgs/TaskUpdate` messages.
+    Takes in information from the `angel_msgs/ActivityDetection`,
+    `angel_msgs/ObjectDetection3dSet`, and `angel_msgs/TaskUpdate` messages.
 
-    Publishes `angel_msgs/AruiUpdate` representing the current activity, detections, and task.
+    Publishes `angel_msgs/AruiUpdate` representing the current activity,
+    detections, and task.
     """
 
     def __init__(self):
         super().__init__(self.__class__.__name__)
         self.log = self.get_logger()
 
-        # Experimenting with the "correct" way to safely declare parameters and
-        # check for when they are not set to anything when lacking default
-        # values. This doesn't seem to normally happen: some default value is
-        # set behind the scenes, like an empty string. There is however a type
-        # enum for parameters that has a "NOT_SET" option to know when a
-        # parameter has not been given. Trying to use that here.
-        parameter_names = [
-            PARAM_ACTIVITY_DET_TOPIC,
-            PARAM_OBJECT_DET_TOPIC,
-            PARAM_TASK_MONITOR_TOPIC,
-            PARAM_ARUI_UPDATE_TOPIC,
-            PARAM_INTERP_USER_INTENT_TOPIC,
-        ]
-        set_parameters = self.declare_parameters(
-            namespace="",
-            parameters=[(p,) for p in parameter_names],
+        param_values = declare_and_get_parameters(
+            self,
+            [
+                (PARAM_ACTIVITY_DET_TOPIC,),
+                (PARAM_OBJECT_DET_TOPIC,),
+                (PARAM_TASK_MONITOR_TOPIC,),
+                (PARAM_ARUI_UPDATE_TOPIC,),
+                (PARAM_INTERP_USER_INTENT_TOPIC,),
+                (PARAM_SYSTEM_TEXT_RESPONSE_TOPIC,),
+            ],
         )
-        # Check for not-set parameters
-        some_not_set = False
-        for p in set_parameters:
-            if p.type_ is rclpy.parameter.Parameter.Type.NOT_SET:
-                some_not_set = True
-                self.log.error(f"Parameter not set: {p.name}")
-        if some_not_set:
-            raise ValueError("Some parameters are not set.")
 
-        self._activity_detector_topic = self.get_parameter(
-            PARAM_ACTIVITY_DET_TOPIC
-        ).value
-        self._object_detection_topic = self.get_parameter(PARAM_OBJECT_DET_TOPIC).value
-        self._task_monitor_topic = self.get_parameter(PARAM_TASK_MONITOR_TOPIC).value
-        self._arui_update_topic = self.get_parameter(PARAM_ARUI_UPDATE_TOPIC).value
-        self._interp_uintent_topic = self.get_parameter(
-            PARAM_INTERP_USER_INTENT_TOPIC
-        ).value
-
-        # log inputs for interpreted type and value
-        self.log.info(
-            f"Activity detector topic: "
-            f"({type(self._activity_detector_topic).__name__}) "
-            f"{self._activity_detector_topic}"
-        )
-        self.log.info(
-            f"Object detection topic: "
-            f"({type(self._object_detection_topic).__name__}) "
-            f"{self._object_detection_topic}"
-        )
-        self.log.info(
-            f"Task monitor topic: "
-            f"({type(self._task_monitor_topic).__name__}) "
-            f"{self._task_monitor_topic}"
-        )
-        self.log.info(
-            f"AruiUpdate topic: "
-            f"({type(self._arui_update_topic).__name__}) "
-            f"{self._arui_update_topic}"
-        )
-        self.log.info(
-            f"Interpreted User Intent topic: "
-            f"({type(self._interp_uintent_topic).__name__}) "
-            f"{self._interp_uintent_topic}"
-        )
+        self._activity_detector_topic = param_values[PARAM_ACTIVITY_DET_TOPIC]
+        self._object_detection_topic = param_values[PARAM_OBJECT_DET_TOPIC]
+        self._task_monitor_topic = param_values[PARAM_TASK_MONITOR_TOPIC]
+        self._arui_update_topic = param_values[PARAM_ARUI_UPDATE_TOPIC]
+        self._interp_uintent_topic = param_values[PARAM_INTERP_USER_INTENT_TOPIC]
+        self._system_text_response_topic = param_values[PARAM_SYSTEM_TEXT_RESPONSE_TOPIC]
 
         # subscribers
         self.activity_subscriber = self.create_subscription(
@@ -117,6 +80,13 @@ class FeedbackGenerator(Node):
             InterpretedAudioUserIntent,
             self._interp_uintent_topic,
             self.user_intent_callback,
+            1,
+        )
+
+        self.system_text_subscriber = self.create_subscription(
+            SystemTextResponse,
+            self._system_text_response_topic,
+            self.system_text_response_callback,
             1,
         )
 
@@ -263,6 +233,21 @@ class FeedbackGenerator(Node):
         """
         self.publish_update(intents_for_confirmation=[msg])
 
+    def system_text_response_callback(self, msg: SystemTextResponse) -> None:
+        """
+        Publish an ARUI update message with a *single* ARUI user notification.
+        The ARUI will read the `decription` field aloud.
+        """
+        # Create an AruiUserNotification msg with this information
+        notification = AruiUserNotification()
+
+        notification.category = notification.N_CAT_NOTICE
+        notification.context = notification.N_CONTEXT_USER_MODELING
+
+        notification.title = f"System response for: {msg.utterance_text}"
+        notification.description = f"{msg.response}"
+
+        self.publish_update(notifications=[notification])
 
 def main():
     rclpy.init()
