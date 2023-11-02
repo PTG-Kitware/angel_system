0218b9b94c71e7f7c1eed9dfe98db2b95dde09b5
Josh Anderson <josh.anderson@kitware.com>
2023-11-02 16:06:09 -0500

    Fix display of task steps

--- unity/ARUI/Assets/Scripts/AngelARUIBridge.cs
+++ unity/ARUI/Assets/Scripts/AngelARUIBridge.cs
@@ -80,15 +80,14 @@ public class AngelARUIBridge : MonoBehaviour
         // Update task status
         AngelARUI.Instance.SetCurrentObservedTask(msg.task_update.task_name);
 
-        // Check if the final step is complete
-        if (msg.task_update.completed_steps[msg.task_update.completed_steps.Length - 1])
+        if (msg.task_update.current_step_id == 0 && (msg.task_update.current_step == "background"))
         {
-            // Advance the step ID past the end of the list to tell ARUI the task is done
-            AngelARUI.Instance.GoToStep(msg.task_update.task_name, msg.task_update.current_step_id + 1);
+            // Handle special case going back to background
+            AngelARUI.Instance.GoToStep(msg.task_update.task_name, msg.task_update.current_step_id);
         }
         else
         {
-            AngelARUI.Instance.GoToStep(msg.task_update.task_name, msg.task_update.current_step_id);
+            AngelARUI.Instance.GoToStep(msg.task_update.task_name, msg.task_update.current_step_id + 1);
         }
 
         // Handle user notifications
