0dd94735aeaed5a2fb14fadeec5d7ff54752cec1
Hannah DeFazio <hannah.defazio@kitware.com>
2023-11-17 12:49:22 -0500

    Remove old and unused things

---
+++ angel_system/activity_classification/tcn_hpl/predict.py
@@ -0,0 +1,428 @@
+from dataclasses import dataclass
+from pathlib import Path
+import os
+from threading import RLock
+from typing import List
+from typing import Dict
+from typing import Mapping
+from typing import Optional
+from typing import Sequence
+from typing import Tuple
+
+import kwcoco
+import lightning.fabric.utilities.seed
+import numpy as np
+import numpy.typing as npt
+import torch
+
+from tcn_hpl.data.components.augmentations import NormalizePixelPts
+from tcn_hpl.models.ptg_module import PTGLitModule
+
+from angel_system.activity_classification.utils import (
+    tlbr_to_xywh,
+    obj_det2d_set_to_feature,
+)
+
+
+def load_module(checkpoint_file, label_mapping_file, torch_device) -> PTGLitModule:
+    """
+
+    :param checkpoint_file:
+    :param label_mapping_file:
+    :param torch_device:
+    :return:
+    """
+    # # https://docs.nvidia.com/cuda/cublas/index.html#cublasApi_reproducibility
+    # torch.use_deterministic_algorithms(True)
+    # os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    # lightning.fabric.utilities.seed.seed_everything(12345)
+
+    mapping_file_dir = os.path.abspath(os.path.dirname(label_mapping_file))
+    mapping_file_name = os.path.basename(label_mapping_file)
+    model_device = torch.device(torch_device)
+    model = PTGLitModule.load_from_checkpoint(
+        checkpoint_file,
+        map_location=model_device,
+        # HParam overrides
+        data_dir=mapping_file_dir,
+        mapping_file_name=mapping_file_name,
+    )
+
+    return model
+
+
+@dataclass
+class ObjectDetectionsLTRB:
+    """
+    Expected object detections format for a single frame from the ROS2
+    ecosystem.
+    """
+
+    # Identifier for this set of detections.
+    id: int
+    # Vectorized detection bbox left pixel bounds
+    left: Tuple[float]
+    # Vectorized detection bbox top pixel bounds
+    top: Tuple[float]
+    # Vectorized detection bbox right pixel bounds
+    right: Tuple[float]
+    # Vectorized detection bbox bottom pixel bounds
+    bottom: Tuple[float]
+    # Vectorized detection label of the most confident class.
+    labels: Tuple[str]
+    # Vectorized detection confidence value of the most confidence class.
+    confidences: Tuple[float]
+
+
+def normalize_detection_features(
+    det_feats: npt.ArrayLike,
+    feat_version: int,
+    img_width: int,
+    img_height: int,
+    num_det_classes: int,
+) -> None:
+    """
+    Normalize input object detection descriptor vectors, outputting new vectors
+    of the same shape.
+
+    Expecting input `det_feats` to be in the shape `[window_size, num_feats]'.
+
+    NOTE: This method normalizes in-place, so be sure to clone the input array
+    if that is not desired.
+
+    :param det_feats: Object Detection features to be normalized.
+
+    :return: Normalized object detection features.
+    """
+    # This method is known to normalize in-place.
+    # Shape [window_size, n_feats]
+    NormalizePixelPts(img_width, img_height, num_det_classes, feat_version)(det_feats)
+
+
+def objects_to_feats(
+    frame_object_detections: Sequence[Optional[ObjectDetectionsLTRB]],
+    det_label_to_idx: Dict[str, int],
+    feat_version: int,
+    image_width: int,
+    image_height: int,
+    feature_memo: Optional[Dict[int, npt.NDArray]] = None,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Convert some object detections for some window of frames into a feature
+    vector of version requested.
+
+    :param frame_object_detections: Sequence of object detections for some
+        window of frames. The window size is dictated by this length of this
+        sequence. Some frame "slots" may be None to indicate there were no
+        object detections for that frame.
+    :param det_label_to_idx: Mapping of object detector classes to the
+        activity-classifier input index expectation.
+    :param feat_version: Integer version of the feature vector to generate.
+        See the `obj_det2d_set_to_feature` function for details.
+    :param image_width: Integer pixel width of the image that object detections
+        were generated on.
+    :param image_height: Integer pixel height of the image that object
+        detections were generated on.
+    :param feature_memo: Optional memoization cache to given us that we will
+        access and insert into based on the IDs given to `ObjectDetectionsLTRB`
+        instances encountered.
+
+    :raises ValueError: No non-None object detections in the given input
+        window.
+
+    :return: Window of normalized feature vectors for the given object
+        detections (shape=[window_size, n_feats]), and an appropriate mask
+        vector for use with activity classification (shape=[window_size]).
+    """
+    if all([d is None for d in frame_object_detections]):
+        raise ValueError("No frames with detections in input.")
+
+    feat_memo = {} if feature_memo is None else feature_memo
+
+    window_size = len(frame_object_detections)
+    # Shape [window_size, None|n_feats]
+    feature_list: List[Optional[npt.NDArray]] = [None] * window_size
+    feature_ndim = None
+    feature_dtype = None
+    for i, frame_dets in enumerate(frame_object_detections):
+        frame_dets: ObjectDetectionsLTRB
+        if frame_dets is not None:
+            f_id = frame_dets.id
+            if f_id not in feat_memo:
+                # the input message has tlbr, but obj_det2d_set_to_feature
+                # requires xywh.
+                xs, ys, ws, hs = tlbr_to_xywh(
+                    frame_dets.top,
+                    frame_dets.left,
+                    frame_dets.bottom,
+                    frame_dets.right,
+                )
+                feat = (
+                    obj_det2d_set_to_feature(
+                        frame_dets.labels,
+                        xs,
+                        ys,
+                        ws,
+                        hs,
+                        frame_dets.confidences,
+                        None,
+                        None,
+                        None,
+                        None,
+                        None,
+                        det_label_to_idx,
+                        version=feat_version,
+                    )
+                    .ravel()
+                    .astype(np.float32)
+                )
+                feat_memo[f_id] = feat
+            else:
+                feat = feat_memo[f_id]
+            feature_ndim = feat.shape
+            feature_dtype = feat.dtype
+            feature_list[i] = feat
+    # Already checked that we should have non-zero frames with detections above
+    # so feature_ndim/_dtype should not be None at this stage
+    assert feature_ndim is not None
+    assert feature_dtype is not None
+
+    # Create mask vector, which should indicate which window indices should not
+    # be considered.
+    # NOTE: The expected network is not yet trained to do this, so the mask is
+    #       always 1's right now.
+    # Shape [window_size]
+    mask = torch.ones(window_size)
+
+    # Fill in the canonical "empty" feature vector for those frames that had no
+    # detections.
+    empty_vec = np.zeros(shape=feature_ndim, dtype=feature_dtype)
+    for i in range(window_size):
+        if feature_list[i] is None:
+            feature_list[i] = empty_vec
+
+    # Shape [window_size, n_feats]
+    feature_vec = torch.tensor(feature_list)
+
+    # Normalize features
+    # Shape [window_size, n_feats]
+    normalize_detection_features(
+        feature_vec, feat_version, image_width, image_height, len(det_label_to_idx)
+    )
+
+    return feature_vec, mask
+
+
+def predict(
+    model: PTGLitModule,
+    window_feats: torch.Tensor,
+    mask: torch.Tensor,
+):
+    """
+    Compute model activity classifications, returning a tensor of softmax
+    probabilities.
+
+    We assume the input model and tensors are already on the appropriate
+    device.
+
+    We assume that input features normalized before being provided to this
+    function. See :ref:`normalize_detection_features`.
+
+    The "prediction" of this result can be determined via the `argmax`
+    function::
+
+        proba = predict(model, window_feats, mask)
+        pred = torch.argmax(proba)
+
+    :param model: PTGLitModule instance to use.
+    :param window_feats: Window (sequence) of *normalized* object detection
+        features. Shape: [window_size, feat_dim].
+    :param mask: Boolean array indicating which frames of the input window for
+        the network to consider. Shape: [window_size].
+
+    :return: Probabilities (softmax) of the activity classes.
+    """
+    with torch.no_grad():
+        logits = model(window_feats.T, mask[None, :])
+    # Logits access mirrors model step function argmax access here:
+    #   tcn_hpl.models.ptg_module --> PTGLitModule.model_step
+    # ¯\_(ツ)_/¯
+    return torch.softmax(logits[-1, :, :, -1], dim=1)[0]
+
+
+@dataclass
+class ResultElement:
+    """
+    Single activity classification result element for later inclusion in an
+    output COCO file.
+    """
+
+    # Class index of the single most confident activity class.
+    pred_index: int
+    # Vector of confidence values for all activity classes.
+    conf_vec: List[float]
+
+
+class ResultsCollector:
+    """
+    Utility class to collect TCN-HPL prediction results, encoding the ability
+    to output the results to a COCO file.
+
+    This is a beginning to generalizing results coco file aggregation and
+    output, based on output from `python-tpl/TCN_HPL/tcn_hpl/models/ptg_module.py`
+    in `on_validation_epoch_end` and `on_test_epoch_end` methods.
+
+    TODO: Promote into TCN_HPL repo?
+    """
+
+    def __init__(self, output_filepath: Path, id_to_action: Mapping[int, str]):
+        self._lock = RLock()  # for thread safety
+        self._collection: Dict[int, ResultElement] = {}
+        self._dset = dset = kwcoco.CocoDataset()
+        dset.fpath = output_filepath.as_posix()
+        dset.dataset["info"].append({"activity_labels": id_to_action})
+        self._vid: Optional[int] = None
+
+    def set_video(self, video_name: str) -> None:
+        """
+        Set the video for which we are currently collecting results for.
+
+        :param video_name: Semantic name of the video   .
+        """
+        with self._lock:
+            video_lookup = self._dset.index.name_to_video
+            if video_name in video_lookup:
+                self._vid = video_lookup[video_name]["id"]
+            else:
+                self._vid = self._dset.add_video(name=video_name)
+
+    def collect(
+        self,
+        frame_index: int,
+        activity_pred: int,
+        activity_conf_vec: Sequence[float],
+        name: Optional[str] = None,
+        file_name: Optional[str] = None,
+        activity_gt: Optional[int] = None,
+    ) -> None:
+        """
+        See `CocoDataset.add_image` for more details.
+        """
+        with self._lock:
+            if self._vid is None:
+                raise RuntimeError(
+                    "No video set before results collection. See `set_video` method."
+                )
+            packet = dict(
+                video_id=self._vid,
+                frame_index=frame_index,
+                activity_pred=activity_pred,
+                activity_conf=list(activity_conf_vec),
+            )
+            if name is not None:
+                packet["name"] = name
+            if file_name is not None:
+                packet["file_name"] = file_name
+            if activity_gt is not None:
+                packet["activity_gt"] = activity_gt
+            self._dset.add_image(**packet)
+
+    def write_file(self):
+        """
+        Write COCO file to the set output path.
+        """
+        with self._lock:
+            dset = self._dset
+            dset.dump(dset.fpath, newlines=True)
+
+
+###############################################################################
+# Functions for debugging things in an interpreter
+#
+def windows_from_all_feature(
+    all_features: npt.ArrayLike, window_size: int
+) -> npt.ArrayLike:
+    """
+    Iterate over overlapping windows in the frame detections features given.
+
+    :param all_features: All object detection feature vectors for all frames to
+        consider. Shape: [n_frames, n_feats]
+    :param window_size: Size of the window to slide.
+
+    :return: Generator yielding different windows of feature vectors.
+    """
+    i = 0
+    stride = 1
+    while (i + window_size) < np.shape(all_features)[0]:
+        yield all_features[i : (i + window_size), :]
+        i += stride
+
+
+def debug_from_array_file() -> None:
+    import functools
+    import re
+    import numpy as np
+    import torch
+    from tqdm import tqdm
+    from tcn_hpl.data.components.augmentations import NormalizePixelPts
+    from angel_system.tcn_hpl.predict import (
+        load_module,
+        predict,
+        windows_from_all_feature,
+    )
+
+    # Pre-computed, un-normalized features per-frame extracted from the
+    # training harness, in temporally ascending order.
+    # Shape = [n_frames, n_feats]
+    all_features = torch.tensor(
+        np.load("./model_files/all_activities_20.npy").astype(np.float32).T
+    ).to("cuda")
+
+    model = load_module(
+        "./model_files/activity_tcn-coffee-checkpoint.ckpt",
+        "./model_files/activity_tcn-coffee-mapping.txt",
+        "cuda",
+    ).eval()
+
+    # Above model window size = 30
+    mask = torch.ones(30).to("cuda")
+
+    # Normalize features
+    # The `objects_to_feats` above includes normalization along with the
+    # bounding box conversion, so this needs to be applied explicitly outside
+    # using `objects_to_feats` (though, using the same normalize func).
+    norm_func = functools.partial(
+        normalize_detection_features,
+        feat_version=5,
+        img_width=1280,
+        img_height=720,
+        num_det_classes=42,
+    )
+
+    # Shape [n_windows, window_size, n_feats]
+    all_windows = list(windows_from_all_feature(all_features, 30))
+
+    all_proba = list(
+        tqdm(
+            (predict(model, norm_func(w.clone()), mask) for w in all_windows),
+            total=len(all_windows),
+        )
+    )
+
+    all_preds_idx = np.asarray([int(torch.argmax(p)) for p in all_proba])
+    all_preds_lbl = [model.classes[p] for p in all_preds_idx]
+
+    # Load Hannah preds
+    comparison_preds_file = "./model_files/all_activities_20_preds.txt"
+    re_window_pred = re.compile(r"^gt: (\d+), pred: (\d+)$")
+    comparison_gt = []
+    comparison_preds_idx = []
+    with open(comparison_preds_file) as infile:
+        for l in infile.readlines():
+            m = re_window_pred.match(l.strip())
+            comparison_gt.append(int(m.groups()[0]))
+            comparison_preds_idx.append(int(m.groups()[1]))
+    comparison_preds_idx = np.asarray(comparison_preds_idx)
+
+    ne_mask = all_preds_idx != comparison_preds_idx
+    all_preds_idx[ne_mask], comparison_preds_idx[ne_mask]
---
+++ angel_system/activity_classification/train_activity_classifier.py
@@ -0,0 +1,391 @@
+import os
+import yaml
+import pickle
+import kwcoco
+import argparse
+import json
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from typing import Union, Tuple, List
+from pathlib import Path, PosixPath
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import preprocessing
+from sklearn.metrics import average_precision_score
+
+from angel_system.data.common.load_data import sanitize_str
+
+from angel_system.activity_classification.utils import (
+    obj_det2d_set_to_feature,
+)
+
+
+def data_loader(
+    dset: Union[str, PosixPath, kwcoco.CocoDataset], act_labels: dict
+) -> Tuple[dict, dict, dict, dict, dict, dict, dict]:
+    """Parse the data in ``dset``
+
+    :param dset: kwcoco dataset
+    :param act_labels: The activity labels
+
+    :return:
+        - act_map: Activity label string to id dict
+        - inv_act_map: Activity id to label string dict
+        - image_activity_gt: Image id to activity label string dict
+        - image_id_to_dataset: Image id to id in ``dset`` dict
+        - label_to_ind: Object detection labels to ids dict
+        - act_id_to_str: Object detection ids to labels dict
+        - ann_by_image: Image id to annotation dict
+    """
+    print("Loading data....")
+    # Description to ID map.
+    act_map = {}
+    inv_act_map = {}
+    for step in act_labels["labels"]:
+        act_map[sanitize_str(step["label"])] = step["id"]
+        inv_act_map[step["id"]] = step["label"]
+
+    if 0 not in act_map.values():
+        act_map["background"] = 0
+        inv_act_map[0] = "background"
+
+    # Load object detections
+    if type(dset) == str or type(dset) == PosixPath:
+        dset_fn = dset
+        dset = kwcoco.CocoDataset(dset_fn)
+        print(f"Loaded dset from file: {dset_fn}")
+        dset.fpath = dset_fn
+
+    image_activity_gt = {}
+    image_id_to_dataset = {}
+    for img_id in dset.imgs:
+        im = dset.imgs[img_id]
+        gid = im["id"]
+        image_id_to_dataset[gid] = os.path.split(im["file_name"])[0]
+
+        activity_gt = im["activity_gt"]
+        if activity_gt is None:
+            activity_gt = "background"  # continue
+
+        image_activity_gt[gid] = act_map[sanitize_str(activity_gt)]
+
+    dsets = sorted(list(set(image_id_to_dataset.values())))
+    image_id_to_dataset = {
+        i: dsets.index(image_id_to_dataset[i]) for i in image_id_to_dataset
+    }
+
+    min_cat = min([dset.cats[i]["id"] for i in dset.cats])
+    num_act = len(dset.cats)
+    label_to_ind = {
+        dset.cats[i]["name"]: dset.cats[i]["id"] - min_cat for i in dset.cats
+    }
+    print(
+        f"Object label mapping:\n\t"
+        f"{json.dumps([o['name'] for o in dset.categories().objs])}"
+    )
+    act_id_to_str = {dset.cats[i]["id"]: dset.cats[i]["name"] for i in dset.cats}
+
+    ann_by_image = {}
+    for gid, anns in dset.index.gid_to_aids.items():
+        ann_by_image[gid] = []
+        for ann_id in anns:
+            ann = dset.anns[ann_id]
+            ann_by_image[gid].append(ann)
+
+    return (
+        act_map,
+        inv_act_map,
+        image_activity_gt,
+        image_id_to_dataset,
+        label_to_ind,
+        act_id_to_str,
+        ann_by_image,
+    )
+
+
+def compute_feats(
+    act_map: dict,
+    image_activity_gt: dict,
+    image_id_to_dataset: dict,
+    label_to_ind: dict,
+    act_id_to_str: dict,
+    ann_by_image: dict,
+    feat_version=1,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Compute features from object detections
+
+    :param act_map: Activity label string to id
+    :param image_activity_gt: Image id to activity label string dict
+    :param image_id_to_dataset: Image id to id in ``dset`` dict
+    :param label_to_ind: Object detection labels to ids dict
+    :param act_id_to_str: Object detection ids to labels dict
+    :param ann_by_image: Image id to annotation dict
+
+    :return: resulting feature data and its labels
+    """
+    print("Computing features...")
+    X = []
+    y = []
+    dataset_id = []
+    last_dset = 0
+
+    for image_id in sorted(list(ann_by_image.keys())):
+        label_vec = []
+        xs = []
+        ys = []
+        ws = []
+        hs = []
+        label_confidences = []
+        obj_obj_contact_state = []
+        obj_obj_contact_conf = []
+        obj_hand_contact_state = []
+        obj_hand_contact_conf = []
+
+        for ann in ann_by_image[image_id]:
+            label_vec.append(act_id_to_str[ann["category_id"]])
+            xs.append(ann["bbox"][0])
+            ys.append(ann["bbox"][1])
+            ws.append(ann["bbox"][2])
+            hs.append(ann["bbox"][3])
+            label_confidences.append(ann["confidence"])
+
+            try:
+                obj_obj_contact_state.append(ann["obj-obj_contact_state"])
+                obj_obj_contact_conf.append(ann["obj-obj_contact_conf"])
+                obj_hand_contact_state.append(ann["obj-hand_contact_state"])
+                obj_hand_contact_conf.append(ann["obj-hand_contact_conf"])
+            except KeyError:
+                pass
+
+        feature_vec = obj_det2d_set_to_feature(
+            label_vec,
+            xs,
+            ys,
+            ws,
+            hs,
+            label_confidences,
+            None,
+            obj_obj_contact_state,
+            obj_obj_contact_conf,
+            obj_hand_contact_state,
+            obj_hand_contact_conf,
+            label_to_ind,
+            version=feat_version,
+        )
+
+        X.append(feature_vec.ravel())
+        try:
+            dataset_id.append(image_id_to_dataset[image_id])
+            last_dset = dataset_id[-1]
+        except:
+            dataset_id.append(last_dset)
+
+        try:
+            y.append(image_activity_gt[image_id])
+        except:
+            y.append(0)
+
+    X = np.array(X)
+    y = np.array(y)
+    dataset_id = np.array(dataset_id)
+
+    return X, y
+
+
+def plot_dataset_counts(
+    X: np.ndarray, y: np.ndarray, output_dir: Union[str, PosixPath], split: str
+):
+    """Plot the number of ground truth steps in the data
+
+    :param X: feature vector from object detections per image,
+        shape is (# frames x # object detection labels)
+    :param y: the target activity ids,
+        shape is (# frames,)
+    :param output_dir: Directory to save the plot to
+    :param split: Which test set ``X`` and ``y`` come from. This is
+        used to create a subfolder under ``output_dir``
+    """
+    plt.imshow(np.cov(X.T))
+    plt.colorbar()
+
+    y_ = y.tolist()
+    x_ = list(range(max(y) + 1))
+    counts = [y_.count(i) for i in x_]
+    plt.close("all")
+    fig = plt.figure(num=None, figsize=(14, 10), dpi=80)
+    plt.rc("font", **{"size": 22})
+    plt.rc("axes", linewidth=4)
+    plt.bar(x_, counts)
+    plt.xticks(x_)
+    plt.ylabel("Counts", fontsize=34)
+    plt.xlabel("Ground Truth Steps", fontsize=34)
+    plt.tight_layout()
+
+    plot_dir = f"{output_dir}/{split}"
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+    plt.savefig(f"{plot_dir}/gt_counts.png")
+
+
+def train(X: np.ndarray, y: np.ndarray) -> RandomForestClassifier:
+    """Train the random forest classifier
+
+    :param X: feature vector from object detections per image,
+        shape is (# frames x # object detection labels)
+    :param y: the target activity ids,
+        shape is (# frames,)
+
+    :return: The trained random forest classifier
+    """
+    print("x", type(X), X.shape)
+    print("y", type(y), y.shape)
+    print("Train...")
+    # Train model on test set.
+    clf = RandomForestClassifier(
+        max_depth=10,
+        random_state=0,
+        n_estimators=1000,
+        max_features=0.1,
+        bootstrap=True,
+        verbose=True,
+    )
+    clf.fit(X, y)
+
+    return clf
+
+
+def validate(
+    clf: RandomForestClassifier, X_final_test: np.ndarray, y_final_test: np.ndarray
+) -> float:
+    """Calculate the average precision of ``clf``
+
+    :param clf: model
+    :param X_final_test: feature vector from object detections per image,
+        shape is (# frames x # object detection labels)
+    :param y_final_test: the target activity ids,
+        shape is (# frames,)
+
+    :return: Average precision score
+    """
+    y_score = clf.predict_proba(X_final_test)  # num data pts x num classes
+
+    # Convert column vector to indicator vector
+    lb = preprocessing.LabelBinarizer()
+    y_true = lb.fit(range(y_score.shape[1])).transform(y_final_test)
+
+    s = average_precision_score(y_true, y_score)
+    print(f"Average precision: {s}")
+
+    return s
+
+
+def save(
+    output_dir: Union[str, PosixPath],
+    act_str_list: List[str],
+    label_to_ind: dict,
+    clf: RandomForestClassifier,
+):
+    """Save the model to a pickle file
+
+    :param output_dir: Path to save the model to
+    :param act_str_list: List of activity label strings
+    :param label_to_ind: Object detection labels to ids dict
+    :param clf: model
+    """
+    output_fn = f"{output_dir}/activity_weights.pkl"
+    with open(output_fn, "wb") as of:
+        pickle.dump([label_to_ind, 1, clf, act_str_list], of)
+    print(f"Saved weights to {output_fn}")
+
+
+def train_activity_classifier(args: argparse.Namespace):
+    """Load the data and train an activity classifier
+
+    :param args: Input arguments
+    """
+    # Load labels
+    with open(args.act_label_yaml, "r") as stream:
+        print(f"Loading activity labels from: {args.act_label_yaml}")
+        act_labels = yaml.safe_load(stream)
+
+    # Load train dataset
+    (
+        act_map,
+        inv_act_map,
+        image_activity_gt,
+        image_id_to_dataset,
+        label_to_ind,
+        act_id_to_str,
+        ann_by_image,
+    ) = data_loader(args.train_fn, act_labels)
+    X, y = compute_feats(
+        act_map,
+        image_activity_gt,
+        image_id_to_dataset,
+        label_to_ind,
+        act_id_to_str,
+        ann_by_image,
+    )
+    plot_dataset_counts(X, y, args.output_dir, "train")
+
+    # Load validation dataset
+    (
+        val_act_map,
+        val_inv_act_map,
+        val_image_activity_gt,
+        val_image_id_to_dataset,
+        val_label_to_ind,
+        val_act_id_to_str,
+        val_ann_by_image,
+    ) = data_loader(args.val_fn, act_labels)
+    X_final_test, y_final_test = compute_feats(
+        val_act_map,
+        val_image_activity_gt,
+        val_image_id_to_dataset,
+        val_label_to_ind,
+        val_act_id_to_str,
+        val_ann_by_image,
+    )
+    plot_dataset_counts(X_final_test, y_final_test, args.output_dir, "val")
+
+    # Train
+    clf = train(X, y)
+    ap = validate(clf, X_final_test, y_final_test)
+
+    # Save
+    act_str_list = [inv_act_map[key] for key in sorted(list(set(y)))]
+    save(args.output_dir, act_str_list, label_to_ind, clf)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--train",
+        help="Object detections in kwcoco format for the train set",
+        dest="train_fn",
+        type=Path,
+    )
+    parser.add_argument(
+        "--val",
+        help="Object detections in kwcoco format for the validation set",
+        dest="val_fn",
+        type=Path,
+    )
+    parser.add_argument(
+        "--act-label-yaml", help="", type=Path, dest="act_label_yaml", default=""
+    )
+    parser.add_argument(
+        "--output-dir", help="", type=Path, dest="output_dir", default=""
+    )
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    train_activity_classifier(args)
+
+
+if __name__ == "__main__":
+    main()
