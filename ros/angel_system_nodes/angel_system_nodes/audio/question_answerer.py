0215a265cd7d288dab1ac95f2f6afe320f1d8e1b
derkmed <derekhmd@gmail.com>
2023-12-20 23:47:11 -0500

    Migrate audio nodes to rely on DialogueUtterance as input/output messages

--- ros/angel_system_nodes/angel_system_nodes/audio/question_answerer.py
+++ ros/angel_system_nodes/angel_system_nodes/audio/question_answerer.py
@@ -7,7 +7,7 @@ import requests
 from termcolor import colored
 import threading
 
-from angel_msgs.msg import InterpretedAudioUserEmotion, SystemTextResponse
+from angel_msgs.msg import DialogueUtterance, SystemTextResponse
 from angel_utils import declare_and_get_parameters
 from angel_utils import make_default_main
 
@@ -15,9 +15,10 @@ from angel_utils import make_default_main
 openai.organization = os.getenv("OPENAI_ORG_ID")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-IN_EMOTION_TOPIC = "user_emotion_topic"
+INPUT_TOPIC = "input_topic"
 OUT_QA_TOPIC = "system_text_response_topic"
 FEW_SHOT_PROMPT = "few_shot_prompt_file"
+PARAM_TIMEOUT = "timeout"
 
 
 class QuestionAnswerer(Node):
@@ -28,14 +29,15 @@ class QuestionAnswerer(Node):
         param_values = declare_and_get_parameters(
             self,
             [
-                (IN_EMOTION_TOPIC,),
+                (INPUT_TOPIC,),
                 (OUT_QA_TOPIC,),
                 (FEW_SHOT_PROMPT,),
             ],
         )
-        self._in_emotion_topic = param_values[IN_EMOTION_TOPIC]
+        self._input_topic = param_values[INPUT_TOPIC]
         self._out_qa_topic = param_values[OUT_QA_TOPIC]
         self.prompt_file = param_values[FEW_SHOT_PROMPT]
+        self.timeout = param_values[PARAM_TIMEOUT]
 
         self.question_queue = queue.Queue()
         self.handler_thread = threading.Thread(target=self.process_question_queue)
@@ -59,8 +61,8 @@ class QuestionAnswerer(Node):
 
         # Handle subscription/publication topics.
         self.subscription = self.create_subscription(
-            InterpretedAudioUserEmotion,
-            self._in_emotion_topic,
+            DialogueUtterance,
+            self._input_topic,
             self.question_answer_callback,
             1,
         )
@@ -68,28 +70,25 @@ class QuestionAnswerer(Node):
             SystemTextResponse, self._out_qa_topic, 1
         )
 
-    def get_response(self, user_utterance: str, user_emotion: str):
+    def get_response(self, sub_msg: DialogueUtterance):
         """
-        Generate a  response to the utterance, enriched with the addition of
-        the user's detected emotion. Inference calls can be added and revised
-        here.
+        Generate a response to the received message.
+        Inference calls can be added and revised here.
         """
-        return_msg = ""
         try:
             if self.is_openai_ready:
                 return_msg = colored(
-                    self.prompt_gpt(user_utterance) + "\n", "light_green"
+                    self.prompt_gpt(sub_msg.utterance_text) + "\n", "light_green"
                 )
         except RuntimeError as err:
             self.log.info(err)
             colored_apology = colored(
                 "I'm sorry. I don't know how to answer your statement.", "light_red"
             )
-            colored_emotion = colored(user_emotion, "light_red")
+            colored_emotion = colored(sub_msg.emotion, "light_red")
             return_msg = (
                 f"{colored_apology} I understand that you feel {colored_emotion}."
             )
-        return return_msg
 
     def question_answer_callback(self, msg):
         """
@@ -108,22 +107,22 @@ class QuestionAnswerer(Node):
         while True:
             msg = self.question_queue.get()
             emotion = msg.user_emotion
-            response = self.get_response(msg.utterance_text, emotion)
-            self.publish_generated_response(msg.utterance_text, response)
+            response = self.get_response(msg)
+            self.publish_generated_response(msg, response)
 
-    def publish_generated_response(self, utterance: str, response: str):
-        msg = SystemTextResponse()
-        msg.header.frame_id = "GPT Question Answering"
-        msg.header.stamp = self.get_clock().now().to_msg()
-        msg.utterance_text = utterance
-        msg.response = response
-        colored_utterance = colored(utterance, "light_blue")
+    def publish_generated_response(self, sub_msg: DialogueUtterance, response: str):
+        pub_msg = SystemTextResponse()
+        pub_msg.header.frame_id = "GPT Question Answering"
+        pub_msg.header.stamp = self.get_clock().now().to_msg()
+        pub_msg.utterance_text = sub_msg.utterance_text
+        pub_msg.response = response
+        colored_utterance = colored(sub_msg.utterance_text, "light_blue")
         colored_response = colored(response, "light_green")
         self.log.info(
             f'Responding to utterance:\n>>> "{colored_utterance}"\n>>> with:\n'
             + f'>>> "{colored_response}"'
         )
-        self._qa_publisher.publish(msg)
+        self._qa_publisher.publish(pub_msg)
 
     def prompt_gpt(self, question, model: str = "gpt-3.5-turbo"):
         prompt = self.prompt.format(question)
@@ -138,6 +137,7 @@ class QuestionAnswerer(Node):
             "https://api.openai.com/v1/chat/completions",
             json=payload,
             headers={"Authorization": "Bearer {}".format(self.openai_api_key)},
+            timeout=self.timeout,
         )
         return (
             json.loads(req.text)["choices"][0]["message"]["content"]
