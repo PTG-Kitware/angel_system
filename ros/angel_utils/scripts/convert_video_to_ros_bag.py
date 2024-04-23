62b8c2fc5764123323f48b689d9e3237bc19eb2e
Hannah DeFazio <hannah.defazio@gyges.khq.kitware.com>
2024-04-23 13:32:24 -0400

    Add docstring, refactor arguments

--- ros/angel_utils/scripts/convert_video_to_ros_bag.py
+++ ros/angel_utils/scripts/convert_video_to_ros_bag.py
@@ -10,12 +10,16 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from rclpy.serialization import serialize_message
 
-NAMESPACE = "/kitware/"
 
+def convert_video_to_bag(video_fn, output_bag_folder, output_image_topic="/kitware/image_0"):
+    """Convert a mp4 video to a ros bag
 
-def convert_video_to_bag(video_fn, bag_folder, topic_name="image_0"):
+    :param video_fn: Path to an mp4 video file
+    :param output_bag_folder: Path to the folder that will be created to contain the ROS bag
+    :param output_image_topic: ROS topic to publish the images to in the ROS bag. Must include the namespace
+    """
     # Create output bag
-    storage_options = rosbag2_py.StorageOptions(uri=bag_folder, storage_id="sqlite3")
+    storage_options = rosbag2_py.StorageOptions(uri=output_bag_folder, storage_id="sqlite3")
     serialization_fmt = "cdr"
     converter_options = rosbag2_py.ConverterOptions(
         input_serialization_format=serialization_fmt,
@@ -26,7 +30,7 @@ def convert_video_to_bag(video_fn, bag_folder, topic_name="image_0"):
     bag_writer.open(storage_options, converter_options)
     bag_writer.create_topic(
         rosbag2_py.TopicMetadata(
-            name=NAMESPACE + topic_name,
+            name=output_image_topic,
             type="sensor_msgs/msg/Image",
             serialization_format=serialization_fmt,
         )
@@ -60,14 +64,14 @@ def convert_video_to_bag(video_fn, bag_folder, topic_name="image_0"):
         # Write to bag
         try:
             bag_writer.write(
-                NAMESPACE + topic_name,
+                output_image_topic,
                 serialize_message(image_msg),
                 frame_ts.nanoseconds,
             )
         except Exception as err:
             # Truncating the error message because it printed out the whole image_msg input
             print("error", type(err), str(err)[:400])
-            return
+            exit(1)
 
         frame_id += 1
 
@@ -81,18 +85,24 @@ def main():
         "--video-fn",
         type=str,
         default="video123.mp4",
-        help=f"Path to a mp4 video",
+        help=f"Path to an mp4 video file",
     )
     parser.add_argument(
-        "--bag-folder",
+        "--output-bag-folder",
         type=str,
         default="video123",
-        help=f"Path to the ros bag folder that will be created",
+        help=f"Path to the folder that will be created to contain the ROS bag",
+    )
+    parser.add_argument(
+        "--output-image-topic",
+        type=str,
+        default="/kitware/image_0",
+        help=f"ROS topic to publish the images to in the ROS bag. Must include the namespace",
     )
 
     args = parser.parse_args()
 
-    convert_video_to_bag(args.video_fn, args.bag_folder)
+    convert_video_to_bag(args.video_fn, args.output_bag_folder, args.output_image_topic)
 
 
 if __name__ == "__main__":
