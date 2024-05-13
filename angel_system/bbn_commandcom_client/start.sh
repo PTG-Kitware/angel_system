f5e25326d0bedcb74b463d574abd57ad468ffa0d
Paul Tunison <paul.tunison@kitware.com>
2024-05-13 14:07:23 -0400

    Update commandcom start script to be a little more robust to exec location

--- angel_system/bbn_commandcom_client/start.sh
+++ angel_system/bbn_commandcom_client/start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 BBN_COMMANDCOM_URI="tcp://128.33.193.178:5558"
 
-./angel-workspace-shell.sh -r \
+# We know that the workspace-shell script is two levels above here.
+"${SCRIPT_DIR}"/../../angel-workspace-shell.sh -r \
   python3 "/angel_workspace/angel_system/bbn_commandcom_client/client.py" \
   --address "${BBN_COMMANDCOM_URI}" \
   --name kw \
-  --skill-config m2  demos/medical/BBN-M2-Tourniquet \
-  --skill-config m3  demos/medical/M3-BBN-integrate-Kitware \
+  --skill-config m2  demos/medical/BBN-M2 \
+  --skill-config m3  demos/medical/BBN-M3 \
+  --skill-config m5  demos/medical/BBN-M5 \
   --skill-config r18 demos/medical/BBN-R18
