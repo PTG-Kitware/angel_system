34201a53404767373df9d1ac702a11bb2e78a798
Hannah DeFazio <hannah.defazio@gyges.khq.kitware.com>
2024-05-16 16:56:27 -0400

    Fix variable name

--- tests/angel_system/impls/detect_activities/detections_to_activities/test_utils.py
+++ tests/angel_system/impls/detect_activities/detections_to_activities/test_utils.py
@@ -166,7 +166,7 @@ class TestUtil_obj_det2d_set_to_feature_by_method_v5:
                 "water jug lid": 4,
             },
             version=5,
-            top_n_objects=1,
+            top_k_objects=1,
         )
 
         feature_vec = obj_det2d_set_to_feature(**test_input)
