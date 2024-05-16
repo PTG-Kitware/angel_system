d32f512baf3ea7e3b0d8a5b81541d99edf847cbd
Hannah DeFazio <hannah.defazio@gyges.khq.kitware.com>
2024-05-16 17:13:36 -0400

    Black formatting

--- tests/angel_system/impls/detect_activities/detections_to_activities/test_utils.py
+++ tests/angel_system/impls/detect_activities/detections_to_activities/test_utils.py
@@ -18,7 +18,9 @@ class TestUtil_obj_det2d_set_to_feature_by_method_v5:
         """
         Testing that object distance to hand is output correctly.
         """
-        norm = NormalizePixelPts(1280, 720, num_obj_classes=124, feat_version=5, top_k_objects=1)
+        norm = NormalizePixelPts(
+            1280, 720, num_obj_classes=124, feat_version=5, top_k_objects=1
+        )
 
         test_input = dict(
             label_vec=[
